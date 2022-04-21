08a85293faffbf99f5ec26ddb08faa11c60b2f8d
Sukhwan Mun <shmun@google.com>
2022-04-21 14:09:02 +0000

    Add the license comment
    This is to add the license comment
    
    Test: build
    Change-Id: I2df0663e1309e355210310641728ec27596a4060

--- native/libimsstack/platform/interface/IIPSecPolicyListener.h
+++ native/libimsstack/platform/interface/IIPSecPolicyListener.h
@@ -1,18 +1,20 @@
-
 /*
-    Author
-    <table>
-    date      author                    description
-    --------  --------------            ----------
-    20101202  sukhwan.mun@              Created
-    </table>
-
-    Description
-
-*/
-
-#ifndef _INTERFACE_IPSEC_POLICY_LISTENER_H_
-#define _INTERFACE_IPSEC_POLICY_LISTENER_H_
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef INTERFACE_IPSEC_POLICY_LISTENER_H_
+#define INTERFACE_IPSEC_POLICY_LISTENER_H_
 
 #include "IMSTypeDef.h"
 
@@ -40,4 +42,4 @@ public:
     virtual void ExpiredSAs(IN IIPSecPolicy* piNetIPSec) = 0;
 };
 
-#endif // _INTERFACE_IPSEC_POLICY_LISTENER_H_
+#endif // INTERFACE_IPSEC_POLICY_LISTENER_H_
--- native/libimsstack/platform/interface/util/IPSecPolicy.h
+++ native/libimsstack/platform/interface/util/IPSecPolicy.h
@@ -1,18 +1,21 @@
 
 /*
-    Author
-    <table>
-    date      author                    description
-    --------  --------------            ----------
-    20101202  sukhwan.mun@              Created
-    </table>
-
-    Description
-
-*/
-
-#ifndef _IPSEC_POLICY_H_
-#define _IPSEC_POLICY_H_
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef IPSEC_POLICY_H_
+#define IPSEC_POLICY_H_
 
 #include "IMSList.h"
 #include "IPAddress.h"
@@ -86,4 +89,4 @@ private:
     IMSList<IPSecSA*> objIPSecSA;
 };
 
-#endif // _IPSEC_POLICY_H_
+#endif // IPSEC_POLICY_H_
