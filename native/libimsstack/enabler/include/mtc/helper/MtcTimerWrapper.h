512c9e1d7ce4bd0130e4e682ac87a2fe2a08c814
doogler <doogler@google.com>
2022-05-11 04:20:35 +0000

    (Mtc) update MtcTimerWrapper
    Updating MtcTimerWrapper
    1. re-start a timer if a timer of the same type is already running
    >> So, the user must check IsAvtive() if it doesn't want refresh.
    
    Test: mm build.
    Change-Id: Ifa4570b59db1b2ba1755ae4c64a34efec9046a46

--- native/libimsstack/enabler/include/mtc/helper/MtcTimerWrapper.h
+++ native/libimsstack/enabler/include/mtc/helper/MtcTimerWrapper.h
@@ -1,8 +1,8 @@
 #ifndef MTC_TIMER_WRAPPER_H_
 #define MTC_TIMER_WRAPPER_H_
 
+#include "ServiceTimer.h"
 #include "IMSList.h"
-#include "IMSMap.h"
 #include "ITimer.h"
 
 class IMtcTimerListener;
@@ -24,24 +24,32 @@ public:
     void StopAll();
     IMS_BOOL IsActive(IN IMS_UINT32 eType);
 
-    typedef struct MtcTimer
+    struct MtcTimer
     {
-        MtcTimer() :
-                eType(0),
-                nDuration(-1),
-                piTimer(IMS_NULL),
-                nId(0)
+        MtcTimer(IN IMS_UINT32 eType) :
+                eType(eType),
+                piTimer(TimerService::GetTimerService()->CreateTimer())
         {
         }
+        ~MtcTimer()
+        {
+            if (piTimer)
+            {
+                piTimer->KillTimer();
+                TimerService::GetTimerService()->DestroyTimer(piTimer);
+            }
+        }
+        MtcTimer(IN const MtcTimer&) = delete;
+        MtcTimer& operator=(IN const MtcTimer&) = delete;
 
         IMS_UINT32 eType;
-        IMS_SINT32 nDuration;
         ITimer* piTimer;
-        IMS_UINTP nId;
-    } MtcTimer;
+    };
 
 private:
+    // TODO: IMSMap<type, timer> would be better.
     IMSList<MtcTimer*> m_lstTimers;
     IMtcTimerListener* m_piListener;
 };
-#endif /*  MTC_TIMER_WRAPPER_H_ */
+
+#endif
