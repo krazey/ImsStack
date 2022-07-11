7c025663c592b6adc0d4161577ab4b3136dcb8fe
jryou <jryou@google.com>
2022-07-11 22:14:28 +0000

    (AoS) Added gtest code for AosHandleMtc #2
    Added gtest code for additional 6 functions in AosHandleMtc
    - Destructor
    - App_Notify
    - CallTracker_StateChanged
    - InitializeServiceBlock
    - InitializeServiceFeature
    - InitializeFeatureTags
    
    Test: IMS reg and gtest w/ cuttlefish
    Change-Id: I704030947e420c6254c464290f79e946b0605b0a

--- tests/native/libimsstack/enabler/aos/handle/AosHandleMtcTest.cpp
+++ tests/native/libimsstack/enabler/aos/handle/AosHandleMtcTest.cpp
@@ -17,13 +17,23 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+#include "CarrierConfig.h"
 #include "ImsAosParameter.h"
+#include "ImsEventDef.h"
+#include "INetworkWatcher.h"
 
 #include "handle/AosHandle.h"
 #include "handle/AosHandleMtc.h"
 #include "interface/IAosAppContext.h"
-#include "interface/MockIAosAppContext.h"
+#include "interface/IAosNConfiguration.h"
+#include "provider/AosProvider.h"
 
+#include "interface/MockIAosAppContext.h"
+#include "interface/MockIAosCallTracker.h"
+#include "interface/MockIAosNConfiguration.h"
+#include "interface/MockIAosNetTracker.h"
+
+using ::testing::_;
 using ::testing::AnyNumber;
 using ::testing::Return;
 using ::testing::ReturnRef;
@@ -32,25 +42,47 @@ class AosHandleMtcTest : public ::testing::Test
 {
 public:
     AosHandleMtc* m_pAosHandleMtc;
-    MockIAosAppContext objMockIAosAppContext;
+
+    MockIAosAppContext m_objMockIAosAppContext;
+    MockIAosNetTracker m_objMockIAosNetTracker;
+
+    IAosNConfiguration* m_piAosNConfiguration;
+    MockIAosNConfiguration m_objMockIAosNConfiguration;
+
+    IAosCallTracker* m_piAosCallTracker;
+    MockIAosCallTracker m_objMockIAosCallTracker;
 
 protected:
     virtual void SetUp() override
     {
-        EXPECT_CALL(objMockIAosAppContext, GetSlotId())
+        EXPECT_CALL(m_objMockIAosAppContext, GetSlotId())
                 .Times(AnyNumber())
                 .WillRepeatedly(Return(0));
 
         const AString strValue = AString("test");
-        EXPECT_CALL(objMockIAosAppContext, GetProfileId())
+        EXPECT_CALL(m_objMockIAosAppContext, GetProfileId())
                 .Times(AnyNumber())
                 .WillRepeatedly(ReturnRef(strValue));
 
-        const AString strAppId = AString("ims.app.mtc");
-        const AString strServiceId = AString("ims.service.mtc");
-        const IMS_UINT32 nServiceType = ImsAosService::MTC;
+        const AString strAppId = AString("ims.app.mtc.test");
+        const AString strServiceId = AString("ims.service.mtc.test");
+        const IMS_UINT32 nServiceType = -1;
 
-        m_pAosHandleMtc = new AosHandleMtc(static_cast<IAosAppContext*>(&objMockIAosAppContext),
+        m_piAosNConfiguration = AosProvider::GetInstance()->GetNConfiguration();
+        AosProvider::GetInstance()->SetNConfiguration(
+                static_cast<IAosNConfiguration*>(&m_objMockIAosNConfiguration));
+
+        EXPECT_CALL(m_objMockIAosAppContext, GetNetTracker())
+                .Times(AnyNumber())
+                .WillRepeatedly(Return(&m_objMockIAosNetTracker));
+
+        m_piAosCallTracker = AosProvider::GetInstance()->GetCallTracker();
+        AosProvider::GetInstance()->SetCallTracker(
+                static_cast<IAosCallTracker*>(&m_objMockIAosCallTracker), 0);
+
+        EXPECT_CALL(m_objMockIAosNConfiguration, SetListener(_)).Times(1);
+
+        m_pAosHandleMtc = new AosHandleMtc(static_cast<IAosAppContext*>(&m_objMockIAosAppContext),
                 strAppId, strServiceId, nServiceType);
 
         ASSERT_TRUE(m_pAosHandleMtc != nullptr);
@@ -58,10 +90,15 @@ protected:
 
     virtual void TearDown() override
     {
-        if (m_pAosHandleMtc)
+        if (m_pAosHandleMtc != nullptr)
         {
             delete m_pAosHandleMtc;
+            m_pAosHandleMtc = nullptr;
         }
+
+        AosProvider::GetInstance()->SetNConfiguration(m_piAosNConfiguration);
+        AosProvider::GetInstance()->SetCallTracker(
+                static_cast<IAosCallTracker*>(m_piAosCallTracker), 0);
     }
 
     IMSMap<IMS_UINT32, IMS_UINT32> GetCapabilities() { return m_pAosHandleMtc->m_objCapabilities; }
@@ -80,9 +117,64 @@ protected:
     {
         return m_pAosHandleMtc->IsBlockForWifi(nBlock);
     }
+
+    void SetHoldingVopsState(IN IMS_UINT32 nState)
+    {
+        m_pAosHandleMtc->m_nHoldingVopsState = nState;
+    }
+
+    IMS_BOOL IsHandleBlocked(IN IMS_UINT32 nBlock)
+    {
+        return m_pAosHandleMtc->AosHandle::IsHandleBlocked(nBlock);
+    }
+
+    IMS_BOOL IsHandleBlocked() { return m_pAosHandleMtc->IsHandleBlocked(); }
+
+    IMS_BOOL IsHandleBlockedBase() { return m_pAosHandleMtc->AosHandle::IsHandleBlocked(); }
+
+    void AddFeature(IN IMS_UINT32 nFeature)
+    {
+        m_pAosHandleMtc->m_objFeatureTagList.AddFeature(nFeature);
+    }
+
+    void AddBindedFeature(IN IMS_UINT32 nFeature)
+    {
+        m_pAosHandleMtc->m_objBindedFeatureTagList.AddFeature(nFeature);
+    }
+
+    IMS_BOOL AddFeatureTag(IN const AString& strName, IN const AString& strValue)
+    {
+        return m_pAosHandleMtc->m_objFeatureTagList.AddFeatureTag(strName, strValue);
+    }
+    IMS_BOOL AddBindedFeatureTag(IN const AString& strName, IN const AString& strValue)
+    {
+        return m_pAosHandleMtc->m_objBindedFeatureTagList.AddFeatureTag(strName, strValue);
+    }
+
+    void SetEpdgEnabled(IN IMS_BOOL bEnabled) { m_pAosHandleMtc->m_bEpdgEnabled = bEnabled; }
+
+    void SetBlocked(IMS_BOOL bBlocked) { m_pAosHandleMtc->m_bBlocked = bBlocked; }
+
+    void AddBlock(IN IMS_UINT32 nBlock)
+    {
+        m_pAosHandleMtc->AddBlock(nBlock, m_pAosHandleMtc->m_nBlocks);
+    }
+
+    void RemoveBlock(IN IMS_UINT32 nBlock)
+    {
+        m_pAosHandleMtc->RemoveBlock(nBlock, m_pAosHandleMtc->m_nBlocks);
+    }
+
+    void InitializeServiceBlock() { m_pAosHandleMtc->InitializeServiceBlock(); }
+
+    void InitializeServiceFeature() { m_pAosHandleMtc->InitializeServiceFeature(); }
+
+    void InitializeFeatureTags() { m_pAosHandleMtc->InitializeFeatureTags(); }
+
+    void UpdateFeatureTags() { m_pAosHandleMtc->UpdateFeatureTags(); }
 };
 
-TEST_F(AosHandleMtcTest, Constructor_AosHandleMtc)
+TEST_F(AosHandleMtcTest, Constructor)
 {
     ASSERT_FALSE(GetCapabilities().GetIndexOfKey(static_cast<IMS_UINT32>(AosNetworkType::LTE)) < 0);
     ASSERT_FALSE(
@@ -113,4 +205,248 @@ TEST_F(AosHandleMtcTest, Constructor_AosHandleMtc)
     EXPECT_TRUE(IsBlockForMobile(AosHandle::BLOCK_VOPS));
     EXPECT_TRUE(IsBlockForWifi(AosHandle::BLOCK_VOWIFI_CAPABILITY));
     EXPECT_TRUE(IsBlockForWifi(AosHandle::BLOCK_VIWIFI_CAPABILITY));
+}
+
+TEST_F(AosHandleMtcTest, Destructor)
+{
+    EXPECT_CALL(m_objMockIAosNConfiguration, RemoveListener(_)).Times(1);
+}
+
+TEST_F(AosHandleMtcTest, App_Notify_Test)
+{
+    m_pAosHandleMtc->App_Notify();
+}
+
+TEST_F(AosHandleMtcTest, CallTracker_StateChanged_Test1)
+{
+    // Test1: call type is not normal
+    // Expectation: do nothing
+
+    m_pAosHandleMtc->CallTracker_StateChanged(IAosCallTracker::TYPE_CS, CallState::RINGING);
+}
+
+TEST_F(AosHandleMtcTest, CallTracker_StateChanged_Test2)
+{
+    // Test2: call type is normal / call state is neither idle nor offhook
+    // Expectation: do nothing
+
+    m_pAosHandleMtc->CallTracker_StateChanged(IAosCallTracker::TYPE_NORMAL, CallState::RINGING);
+}
+
+TEST_F(AosHandleMtcTest, CallTracker_StateChanged_Test3)
+{
+    // Test3: call type is normal / call state is offhook
+    // Expectation: do nothing
+
+    m_pAosHandleMtc->CallTracker_StateChanged(IAosCallTracker::TYPE_CS, CallState::RINGING);
+}
+
+TEST_F(AosHandleMtcTest, CallTracker_StateChanged_Test4)
+{
+    // Test4: Holding vops for call test.
+    //        call type is normal / call state is idle / vops holt for call
+    //        / no unavailable feature policy for vops
+    // Expectation: Add vops block to main blocks
+
+    EXPECT_CALL(m_objMockIAosNConfiguration, IsVopsIgnoredForVolteEnabled())
+            .Times(AnyNumber())
+            .WillRepeatedly(Return(IMS_FALSE));
+
+    IMSVector<IMS_SINT32> objTestVector;
+    EXPECT_CALL(m_objMockIAosNConfiguration, GetRegWithFeatureTagUnavailablePolicy())
+            .Times(AnyNumber())
+            .WillRepeatedly(ReturnRef(objTestVector));
+
+    EXPECT_CALL(m_objMockIAosNConfiguration, GetRegWithFeatureTagUnavailable())
+            .Times(AnyNumber())
+            .WillRepeatedly(ReturnRef(objTestVector));
+
+    EXPECT_CALL(m_objMockIAosNConfiguration,
+            IsGGsmaRcsTelephonyFeatureTagUsedAsAvailableVoiceCallType())
+            .Times(AnyNumber())
+            .WillRepeatedly(Return(IMS_FALSE));
+
+    EXPECT_CALL(m_objMockIAosNetTracker, GetMobileNetworkType())
+            .Times(AnyNumber())
+            .WillRepeatedly(Return(NW_REPORT_RADIO_LTE));
+
+    EXPECT_CALL(m_objMockIAosNConfiguration, IsWfcImsAvailable())
+            .Times(AnyNumber())
+            .WillRepeatedly(Return(IMS_TRUE));
+
+    SetHoldingVopsState(IMS_VOICE_OVER_PS_NOT_SUPPORTED);
+
+    m_pAosHandleMtc->CallTracker_StateChanged(IAosCallTracker::TYPE_NORMAL, CallState::IDLE);
+
+    EXPECT_TRUE(IsHandleBlocked(AosHandle::BLOCK_VOPS));
+}
+
+TEST_F(AosHandleMtcTest, CallTracker_StateChanged_Test5)
+{
+    // Test5: vzw vowifi feature tag test. Idle -> Offhook -> Idle
+    // Expectation: "cs" in idle, "cs,volte" in offhook
+
+    AddFeature(ImsAosFeature::VIDEO);
+    AddFeatureTag("+g.gsma.rcs.telephony", "\"cs\"");
+    AddBindedFeature(ImsAosFeature::VIDEO);
+    AddBindedFeatureTag("+g.gsma.rcs.telephony", "\"cs\"");
+
+    SetEpdgEnabled(IMS_TRUE);
+
+    AosFeatureTagList objExpectedFeatureTagListIdle;
+    objExpectedFeatureTagListIdle.AddFeature(ImsAosFeature::VIDEO);
+    objExpectedFeatureTagListIdle.AddFeatureTag("+g.gsma.rcs.telephony", "\"cs\"");
+
+    AosFeatureTagList objExpectedFeatureTagListOffhook;
+    objExpectedFeatureTagListOffhook.AddFeature(ImsAosFeature::VIDEO);
+    objExpectedFeatureTagListOffhook.AddFeatureTag("+g.gsma.rcs.telephony", "cs");
+    objExpectedFeatureTagListOffhook.AddFeatureTag("+g.gsma.rcs.telephony", "volte");
+
+    EXPECT_CALL(m_objMockIAosNConfiguration, IsVopsIgnoredForVolteEnabled())
+            .Times(AnyNumber())
+            .WillRepeatedly(Return(IMS_TRUE));
+
+    EXPECT_CALL(m_objMockIAosNConfiguration,
+            IsGGsmaRcsTelephonyFeatureTagUsedAsAvailableVoiceCallType())
+            .Times(AnyNumber())
+            .WillRepeatedly(Return(IMS_TRUE));
+
+    EXPECT_CALL(m_objMockIAosCallTracker, IsNormalCallActive())
+            .Times(2)
+            .WillOnce(Return(IMS_TRUE))
+            .WillOnce(Return(IMS_FALSE));
+
+    EXPECT_CALL(m_objMockIAosNConfiguration, IsVideoOverWifiSupportedWithoutVoice())
+            .Times(AnyNumber())
+            .WillRepeatedly(Return(IMS_TRUE));
+
+    EXPECT_CALL(m_objMockIAosNetTracker, GetMobileNetworkType())
+            .Times(AnyNumber())
+            .WillRepeatedly(Return(NW_REPORT_RADIO_GSM));
+
+    m_pAosHandleMtc->CallTracker_StateChanged(IAosCallTracker::TYPE_NORMAL, CallState::OFFHOOK);
+    EXPECT_TRUE(m_pAosHandleMtc->GetFeatureTagList().Equals(objExpectedFeatureTagListOffhook));
+
+    m_pAosHandleMtc->CallTracker_StateChanged(IAosCallTracker::TYPE_NORMAL, CallState::IDLE);
+    EXPECT_TRUE(m_pAosHandleMtc->GetFeatureTagList().Equals(objExpectedFeatureTagListIdle));
+}
+
+/*
+TEST_F(AosHandleMtcTest, NetTracker_StatusChanged_Test)
+{
+    // To do after renewal
+}
+*/
+
+TEST_F(AosHandleMtcTest, InitializeServiceBlock_Test)
+{
+    // Expectation: Set m_bBlocked depending on AosHandleMtc::IsHandleBlocked()
+
+    IMSVector<IMS_SINT32> objTestVector;
+    EXPECT_CALL(m_objMockIAosNConfiguration, GetRegWithFeatureTagUnavailable())
+            .Times(AnyNumber())
+            .WillRepeatedly(ReturnRef(objTestVector));
+
+    AddBlock(AosHandle::BLOCK_VOPS);
+    InitializeServiceBlock();
+    EXPECT_TRUE(IsHandleBlockedBase());
+
+    RemoveBlock(AosHandle::BLOCK_VOPS);
+    InitializeServiceBlock();
+    EXPECT_FALSE(IsHandleBlockedBase());
+}
+
+TEST_F(AosHandleMtcTest, InitializeServiceFeature_Test1)
+{
+    // Test1: All support
+    // Expectation: MMTEL, VIDEO, TEXT, VERSTAT, USSI
+
+    IMSVector<IMS_SINT32> objTestVector;
+    EXPECT_CALL(m_objMockIAosNConfiguration, GetRegWithFeatureTagUnavailable())
+            .Times(AnyNumber())
+            .WillRepeatedly(ReturnRef(objTestVector));
+
+    EXPECT_CALL(m_objMockIAosNConfiguration, IsRttSupported())
+            .Times(AnyNumber())
+            .WillRepeatedly(Return(IMS_TRUE));
+
+    EXPECT_CALL(m_objMockIAosNConfiguration, IsVerstatForRegistrationSupported())
+            .Times(AnyNumber())
+            .WillRepeatedly(Return(IMS_TRUE));
+
+    EXPECT_CALL(m_objMockIAosNConfiguration, GetUssdMethod())
+            .Times(AnyNumber())
+            .WillRepeatedly(Return(CarrierConfig::USSD_OVER_IMS_PREFERRED));
+
+    InitializeServiceFeature();
+
+    EXPECT_TRUE(m_pAosHandleMtc->GetFeatureTagList().HasFeature(ImsAosFeature::MMTEL));
+    EXPECT_TRUE(m_pAosHandleMtc->GetFeatureTagList().HasFeature(ImsAosFeature::VIDEO));
+    EXPECT_TRUE(m_pAosHandleMtc->GetFeatureTagList().HasFeature(ImsAosFeature::TEXT));
+    EXPECT_TRUE(m_pAosHandleMtc->GetFeatureTagList().HasFeature(ImsAosFeature::VERSTAT));
+    EXPECT_TRUE(m_pAosHandleMtc->GetFeatureTagList().HasFeature(ImsAosFeature::USSI));
+}
+
+TEST_F(AosHandleMtcTest, InitializeServiceFeature_Test2)
+{
+    // Test2: No support
+    // Expectation: no features
+
+    IMSVector<IMS_SINT32> objTestVector;
+    EXPECT_CALL(m_objMockIAosNConfiguration, GetRegWithFeatureTagUnavailable())
+            .Times(AnyNumber())
+            .WillRepeatedly(ReturnRef(objTestVector));
+
+    AddBlock(AosHandle::BLOCK_VOLTE_CAPABILITY);
+    AddBlock(AosHandle::BLOCK_VILTE_CAPABILITY);
+
+    EXPECT_CALL(m_objMockIAosNConfiguration, IsRttSupported())
+            .Times(AnyNumber())
+            .WillRepeatedly(Return(IMS_FALSE));
+
+    EXPECT_CALL(m_objMockIAosNConfiguration, IsVerstatForRegistrationSupported())
+            .Times(AnyNumber())
+            .WillRepeatedly(Return(IMS_FALSE));
+
+    EXPECT_CALL(m_objMockIAosNConfiguration, GetUssdMethod())
+            .Times(AnyNumber())
+            .WillRepeatedly(Return(CarrierConfig::USSD_OVER_CS_ONLY));
+
+    InitializeServiceFeature();
+
+    EXPECT_FALSE(m_pAosHandleMtc->GetFeatureTagList().HasFeature(ImsAosFeature::MMTEL));
+    EXPECT_FALSE(m_pAosHandleMtc->GetFeatureTagList().HasFeature(ImsAosFeature::VIDEO));
+    EXPECT_FALSE(m_pAosHandleMtc->GetFeatureTagList().HasFeature(ImsAosFeature::TEXT));
+    EXPECT_FALSE(m_pAosHandleMtc->GetFeatureTagList().HasFeature(ImsAosFeature::VERSTAT));
+    EXPECT_FALSE(m_pAosHandleMtc->GetFeatureTagList().HasFeature(ImsAosFeature::USSI));
+}
+
+TEST_F(AosHandleMtcTest, InitializeFeatureTags_Test)
+{
+    // Expectation: Include +cdmaless, "cs,volte" feature tags
+
+    AddFeature(ImsAosFeature::MMTEL);
+
+    AosFeatureTagList objExpectedFeatureTagList;
+    objExpectedFeatureTagList.AddFeature(ImsAosFeature::MMTEL);
+    objExpectedFeatureTagList.AddFeatureTag("+cdmaless");
+    objExpectedFeatureTagList.AddFeatureTag("+g.gsma.rcs.telephony", "cs");
+    objExpectedFeatureTagList.AddFeatureTag("+g.gsma.rcs.telephony", "volte");
+
+    EXPECT_CALL(m_objMockIAosNConfiguration, IsCdmalessFeatureTagRequired())
+            .Times(1)
+            .WillOnce(Return(IMS_TRUE));
+
+    EXPECT_CALL(m_objMockIAosNConfiguration,
+            IsGGsmaRcsTelephonyFeatureTagUsedAsAvailableVoiceCallType())
+            .Times(1)
+            .WillOnce(Return(IMS_TRUE));
+
+    EXPECT_CALL(m_objMockIAosCallTracker, IsNormalCallActive())
+            .Times(1)
+            .WillOnce(Return(IMS_FALSE));
+
+    InitializeFeatureTags();
+
+    EXPECT_TRUE(m_pAosHandleMtc->GetFeatureTagList().Equals(objExpectedFeatureTagList));
 }
