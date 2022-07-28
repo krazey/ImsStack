7af555dedb3d8e2150c6718ce3d35c24eb656090
Jonggeon Kim <jgkim@google.com>
2022-07-28 06:04:58 +0000

    (Mtc) Unit test - ConferenceInfoManagerTest
    add Unit test for ConferenceInfoManagerTest.
    
    Test: atest ImsStackJavaTests:ConferenceInfoManagerTest
    Change-Id: I9895523c0dd6bb335dfd795f03839160473413c2

---
+++ tests/java/src/com/android/imsstack/enabler/mtc/ConferenceInfoManagerTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.imsstack.enabler.mtc;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class ConferenceInfoManagerTest {
+    private String mCcid = "mCcid";
+    private int mSlotId = 0;
+
+    private ConferenceInfoManager mTestConferenceInfoManager;
+
+    @Before
+    public void setUp() throws Exception {
+        mTestConferenceInfoManager = ConferenceInfoManager.getInstance();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testConferenceInfoManager() {
+        mTestConferenceInfoManager.createConferenceInfo("", mSlotId);
+
+        assertFalse(mTestConferenceInfoManager.hasConferenceInfo());
+
+        mTestConferenceInfoManager.createConferenceInfo(mCcid, mSlotId);
+
+        assertTrue(mTestConferenceInfoManager.hasConferenceInfo());
+        assertNotNull(mTestConferenceInfoManager.getConferenceInfo(mCcid));
+
+        mTestConferenceInfoManager.destroyConferenceInfo(mCcid);
+
+        assertFalse(mTestConferenceInfoManager.hasConferenceInfo());
+        assertNull(mTestConferenceInfoManager.getConferenceInfo(mCcid));
+
+        mTestConferenceInfoManager.createConferenceInfo(mCcid, mSlotId);
+        mTestConferenceInfoManager.createConferenceInfo(mCcid + "1", mSlotId + 1);
+
+        mTestConferenceInfoManager.destroyAllConferenceInfos();
+
+        assertFalse(mTestConferenceInfoManager.hasConferenceInfo());
+        assertNull(mTestConferenceInfoManager.getConferenceInfo(mCcid));
+        assertNull(mTestConferenceInfoManager.getConferenceInfo(mCcid + "1"));
+
+        mTestConferenceInfoManager.createConferenceInfo(mCcid, mSlotId);
+        assertTrue(ConferenceInfoHelper.addConferenceUser(mCcid, mCcid + "1", mCcid + "2", 1));
+
+        assertTrue(mTestConferenceInfoManager.hasConferenceInfo());
+        // TODO : need to be re-considered how to handle 1st param of method below
+        // in the product code.
+        assertNotNull(mTestConferenceInfoManager.getConferenceInfoByUser(1 + "", mCcid + "2"));
+    }
+}
