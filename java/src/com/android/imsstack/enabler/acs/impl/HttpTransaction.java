caf9b8f8a3c1933bbd5e6af427467d9fc4db04eb
hyosun <hyosunkim@google.com>
2022-08-19 08:03:46 +0000

    update HttpTransaction
    Bug: 238376449
    Test: build pass
    Change-Id: Ifbfb3d504590529295c9f627dcbdb9895e6149f9

--- java/src/com/android/imsstack/enabler/acs/impl/HttpTransaction.java
+++ java/src/com/android/imsstack/enabler/acs/impl/HttpTransaction.java
@@ -19,16 +19,26 @@ package com.android.imsstack.enabler.acs.impl;
 import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.content.Context;
+import android.net.Network;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
 import android.os.Message;
+import android.text.TextUtils;
 
+import com.android.imsstack.core.agents.AgentFactory;
+import com.android.imsstack.core.agents.IWifiState;
+import com.android.imsstack.core.agents.dcm.DcFactory;
+import com.android.imsstack.core.agents.dcmif.EApnType;
+import com.android.imsstack.core.agents.dcmif.IDcApn;
 import com.android.imsstack.util.ImsLog;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
+import java.util.Locale;
 
 /**
  * This class provides to send an HTTP GET request to the server and receives an HTTP response.
@@ -64,9 +74,9 @@ public class HttpTransaction {
     // minimum msg
     public static final int MSG_MIN = 0;
     // start msg
-    public static final int MSG_START = 1;
-    // stop msg
-    public static final int MSG_STOP = 2;
+    public static final int REQUEST_START = 1;
+    // send HTTP request
+    public static final int REQUEST_HTTP = 2;
     // send HTTPS request
     public static final int REQUEST_HTTPS = 3;
     // send non-cellular HTTP request using OTP ACS Auth
@@ -105,7 +115,6 @@ public class HttpTransaction {
         int handleMessage(Message msg);
     }
 
-    // Thread
     private final class MessageHandler extends Handler {
         MessageHandler(Looper looper) {
             super(looper);
@@ -125,23 +134,94 @@ public class HttpTransaction {
     private final HashMap<Integer, MessageFunction> mMessageFunctionMap =
             new HashMap<Integer, MessageFunction>() {
                 {
-                    put(MSG_START, mMsgFuncStart);
-                    put(MSG_STOP, mMsgFuncStop);
+                    put(REQUEST_START, mMsgFuncRequestStart);
+                    put(REQUEST_HTTP, mMsgFuncRequestHttp);
+                    put(REQUEST_HTTPS, mMsgFuncRequestHttps);
+                    put(REQUEST_GBA, mMsgFuncRequestGba);
                     put(REQUEST_DONE, mMsgFuncRequestDone);
                 }
             };
 
-    private final MessageFunction mMsgFuncStart = new MessageFunction() {
+    private final MessageFunction mMsgFuncRequestStart = new MessageFunction() {
         @Override
         public int handleMessage(Message msg) {
 
+            // TODO : initial request Type
+            sendMessage(REQUEST_HTTP);
             return 0;
         }
     };
 
-    private final MessageFunction mMsgFuncStop = new MessageFunction() {
+    private final MessageFunction mMsgFuncRequestGba = new MessageFunction() {
         @Override
         public int handleMessage(Message msg) {
+            // TODO: update for GBA
+            mUrl = mRequestInfo.getHttpsUrl();
+
+            // TODO : change apn type
+            String ACSApnType = "mobile_internet";
+            if (!getURLConnection(ACSApnType)) {
+                ImsLog.e(mSlotId, "open UrlConnection is failed");
+                return 0;
+            }
+
+            setConnectionProperties();
+            setAuthorizationHeader();
+//            HttpResponse httpResponse = new HttpResponse(mSlotId, mHttpURLConnection);
+            if (!connect()) {
+                ImsLog.e(mSlotId, "HTTP connection is failed");
+            }
+            return 0;
+        }
+    };
+
+    private final MessageFunction mMsgFuncRequestHttp = new MessageFunction() {
+        @Override
+        public int handleMessage(Message msg) {
+            mUrl = mRequestInfo.getHttpUrl();
+            // TODO : change apn type
+            String ACSApnType = "mobile_internet";
+            if (!getURLConnection(ACSApnType) || mHttpURLConnection == null) {
+                ImsLog.e(mSlotId, "open UrlConnection is failed");
+                return 0;
+            }
+
+            setConnectionProperties();
+            HttpResponse httpResponse = new HttpResponse(mSlotId, mHttpURLConnection);
+            if (!connect()) {
+                ImsLog.e(mSlotId, "HTTP connection is failed");
+            }
+
+            // handle response
+            HttpResponseForCellular httpResponseForCellular = new HttpResponseForCellular(mHandler,
+                    mSlotId);
+            httpResponseForCellular.handle(httpResponse);
+            return 0;
+        }
+    };
+
+    private final MessageFunction mMsgFuncRequestHttps = new MessageFunction() {
+        @Override
+        public int handleMessage(Message msg) {
+            mUrl = mRequestInfo.getHttpsUrl();
+            // TODO : change apn type
+            String ACSApnType = "mobile_internet";
+            if (!getURLConnection(ACSApnType) || mHttpURLConnection == null) {
+                ImsLog.e(mSlotId, "open UrlConnection is failed");
+                return 0;
+            }
+
+            // TODO : consider TLS?
+            setConnectionProperties();
+            HttpResponse httpResponse = new HttpResponse(mSlotId, mHttpURLConnection);
+            if (!connect()) {
+                ImsLog.e(mSlotId, "HTTP connection is failed");
+            }
+
+            // handle response
+            HttpResponseForCellular httpResponseForCellular = new HttpResponseForCellular(mHandler,
+                    mSlotId);
+            httpResponseForCellular.handle(httpResponse);
 
             return 0;
         }
@@ -173,17 +253,23 @@ public class HttpTransaction {
     private final int mSlotId;
     private final int mSubId;
     private final Context mContext;
+    private final RequestInfo mRequestInfo;
 
     private Callback mCallback = null;
+    private HttpURLConnection mHttpURLConnection = null;
+    private URL mUrl = null;
 
-    public HttpTransaction(Context context, int slotId, int subId) {
+    public HttpTransaction(Context context, int slotId, int subId, RequestInfo requestInfo) {
         mContext = context;
         mSlotId = slotId;
         mSubId = subId;
+        mRequestInfo = requestInfo;
         HandlerThread handlerThread = new HandlerThread(HttpTransaction.class.getName());
         handlerThread.start();
 
         mHandler = new MessageHandler(handlerThread.getLooper());
+
+        init();
     }
 
     /**
@@ -206,13 +292,154 @@ public class HttpTransaction {
      * Open a transaction to receive provisioning data from the service provider.
      */
     public void open() {
-
+        sendMessage(REQUEST_START);
     }
 
     /**
      * Close a transaction, since the client no longer needs to send.
      */
     public void close() {
+        if (null != mHttpURLConnection) {
+            mHttpURLConnection.disconnect();
+            mHttpURLConnection = null;
+        }
+        mHandler.getLooper().quit();
+    }
 
+    private void init() {
+        ImsLog.d(mSlotId, "");
+
+        close();
+    }
+
+    private boolean getURLConnection(String acsApnType) {
+        if (null == mUrl) {
+            ImsLog.e(mSlotId, "URL is null");
+            return false;
+        }
+
+        try {
+            Network nw = null;
+            IDcApn dcApn = (IDcApn) DcFactory.getDc(DcFactory.APN, mSlotId);
+
+            if (dcApn == null) {
+                ImsLog.e("DCApn is null");
+                return false;
+            }
+
+            nw = dcApn.getNetworkByCapability(getEApnType(acsApnType));
+
+            if (nw == null) {
+                ImsLog.e(mSlotId, "Network is null");
+                return false;
+            }
+            mHttpURLConnection = (HttpURLConnection) nw.openConnection(mUrl);
+
+        } catch (Exception e) {
+            ImsLog.e(mSlotId, e.getMessage());
+            mHttpURLConnection = null;
+            return false;
+        }
+
+        return true;
+    }
+
+    private int getEApnType(String acsApnType) {
+        IWifiState ws = (IWifiState) AgentFactory.getAgent(AgentFactory.WIFI_STATE, mSlotId);
+        boolean wifiConnected = (ws != null && (!ws.isWifiConnected()) ? true : false);
+
+        int netType = -1;
+        if (TextUtils.isEmpty(acsApnType)) {
+            ImsLog.e(mSlotId, "ApnType is null");
+            return netType;
+        }
+        if (acsApnType.contains("wifi") && wifiConnected) {
+            netType = EApnType.WIFI.getType();
+        } else if (acsApnType.contains("mobile_internet")) {
+            netType = EApnType.INTERNET.getType();
+        } else if (acsApnType.contains("mobile_ims")) {
+            netType = EApnType.IMS.getType();
+        }
+        /* TODO : update vzw app apn
+        else if (AcsApnType.contains("mobile_vzwapp")) {
+            nNetworkType = EApnType.APP.getType();
+        }
+         */
+        ImsLog.d(mSlotId, "Apn " + acsApnType + ", WiFi " + wifiConnected + ", NetType " + netType);
+        return netType;
+    }
+
+    private boolean connect() {
+        boolean result = true;
+
+        int ConnectionTimeoutMillis = 5 * 1000;
+        int TransactionTimeoutMillis = 60 * 1000;
+
+        mHttpURLConnection.setConnectTimeout(ConnectionTimeoutMillis);
+        mHttpURLConnection.setReadTimeout(TransactionTimeoutMillis);
+
+        try {
+            mHttpURLConnection.connect();
+        } catch (Exception e) {
+            ImsLog.e(mSlotId, e.getMessage());
+            result = false;
+        }
+        return result;
+    }
+
+    private boolean setConnectionProperties() {
+        try {
+            // TODO : consider requestType
+            mHttpURLConnection.setRequestMethod("GET");
+            setUserAgent();
+            setLanguageProperty();
+            setAcceptHeader();
+        } catch (Exception e) {
+            ImsLog.e(mSlotId, e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    private void setUserAgent() {
+        if (mRequestInfo == null) {
+            ImsLog.e(mSlotId, "RequestInfo is null");
+            return;
+        }
+
+        mHttpURLConnection.setRequestProperty("User-Agent", mRequestInfo.generateUserAgent());
+    }
+
+    private void setLanguageProperty() {
+        Locale curLocale = Locale.getDefault();
+        String curLanguage = "en";
+        if (curLocale != null) {
+            curLanguage = curLocale.getLanguage();
+        }
+
+        mHttpURLConnection.setRequestProperty("Accept-Language", curLanguage);
+    }
+
+    private void setAcceptHeader() {
+        mHttpURLConnection.setRequestProperty("Accept", "*/*");
+    }
+
+    private void setAuthorizationHeader() {
+        String Auth = "";
+        mHttpURLConnection.setRequestProperty("Authorization", Auth);
+    }
+
+    private void sendMessage(int what) {
+        if (mHandler == null) {
+            ImsLog.e(mSlotId, "handle is null");
+            return;
+        }
+        Message msg = Message.obtain();
+        msg.what = what;
+        msg.arg1 = 0;
+        msg.arg2 = 0;
+        msg.obj = null;
+
+        mHandler.sendMessage(msg);
     }
 }
