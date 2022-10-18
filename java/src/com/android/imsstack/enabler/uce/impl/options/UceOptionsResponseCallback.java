fd6a4eb17063c845aefd30fe056eec4e40a2e7fa
Hyunho <hhshin@google.com>
2022-10-18 03:08:30 +0000

    Add copyright comments to files related to UCE.
    Bug: b/254164655
    Test: build pass
    Change-Id: I40d1d0f87ca2347966624b1a0eac206c36e1c3ef

--- java/src/com/android/imsstack/enabler/uce/impl/options/UceOptionsResponseCallback.java
+++ java/src/com/android/imsstack/enabler/uce/impl/options/UceOptionsResponseCallback.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.android.imsstack.enabler.uce.impl.options;
 
 import android.os.Parcel;
