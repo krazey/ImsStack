f84be2f1ad1db991dd3d77de3292fe66cc57ae20
yongnamcha <yongnamcha@google.com>
2022-11-11 06:16:32 +0000

    (AoS) Add gtest for AosFactory in Native
    This commit adds gtest for AosFactory in Native
    
    Test: atest ImsStackNativeTests
    Change-Id: Ia7770a9cfda7d9aa8356ffe285d957e6dc1f68f4

--- native/libimsstack/enabler/include/aos/manager/AosFactory.h
+++ native/libimsstack/enabler/include/aos/manager/AosFactory.h
@@ -36,7 +36,7 @@ public:
     static void Start(IN IMS_SINT32 nSlotId = IMS_SLOT_0);
     static void Stop(IN IMS_SINT32 nSlotId = IMS_SLOT_0);
 
-private:
+protected:
     static AosFactory* m_gpAosFactory;
     static IMutex* m_gpiLock;
 
---
+++ tests/native/libimsstack/enabler/aos/manager/AosFactoryTest.cpp
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "ImsTypeDef.h"
+#include "manager/AosFactory.h"
+
+class IMutex;
+
+class TestAosFactory : public AosFactory
+{
+    inline TestAosFactory() :
+            AosFactory()
+    {
+    }
+
+public:
+    inline static AosFactory* GetAosFactory() { return AosFactory::m_gpAosFactory; }
+
+    inline static void SetAosFactory(IN AosFactory* gpAosFactory)
+    {
+        AosFactory::m_gpAosFactory = gpAosFactory;
+    }
+
+    inline static IMutex* GetLock() { return AosFactory::m_gpiLock; }
+
+    inline static void SetLock(IN IMutex* gpiLock) { AosFactory::m_gpiLock = gpiLock; }
+
+    inline static IMSMap<IMS_SINT32, ImsAosManager*> GetManagers()
+    {
+        return AosFactory::m_objManagers;
+    }
+
+    inline static void SetManagers(IN IMSMap<IMS_SINT32, ImsAosManager*> objManagers)
+    {
+        AosFactory::m_objManagers = objManagers;
+    }
+
+    inline static void DeleteAosFactory()
+    {
+        if (GetAosFactory())
+        {
+            delete AosFactory::m_gpAosFactory;
+        }
+    }
+};
+
+class AosFactoryTest : public ::testing::Test
+{
+public:
+    AosFactory* m_pOriginAosFactory;
+    IMutex* m_pOriginLock;
+    IMSMap<IMS_SINT32, ImsAosManager*> m_objOriginManagers;
+
+protected:
+    virtual void SetUp() override
+    {
+        m_pOriginAosFactory = TestAosFactory::GetAosFactory();
+        m_pOriginLock = TestAosFactory::GetLock();
+        m_objOriginManagers = TestAosFactory::GetManagers();
+
+        TestAosFactory::SetAosFactory(IMS_NULL);
+        TestAosFactory::SetLock(IMS_NULL);
+        TestAosFactory::GetManagers().Remove(IMS_SLOT_0);
+    }
+
+    virtual void TearDown() override
+    {
+        TestAosFactory::DeleteAosFactory();
+        TestAosFactory::SetAosFactory(IMS_NULL);
+        TestAosFactory::SetLock(IMS_NULL);
+        TestAosFactory::GetManagers().Remove(IMS_SLOT_0);
+
+        TestAosFactory::SetAosFactory(m_pOriginAosFactory);
+        TestAosFactory::SetLock(m_pOriginLock);
+        TestAosFactory::SetManagers(m_objOriginManagers);
+    }
+};
+
+TEST_F(AosFactoryTest, Start)
+{
+    EXPECT_EQ(TestAosFactory::GetAosFactory(), nullptr);
+    EXPECT_EQ(TestAosFactory::GetLock(), nullptr);
+    EXPECT_EQ(TestAosFactory::GetManager(), nullptr);
+    IMS_SINT32 nManagers = TestAosFactory::GetManagers().GetSize();
+
+    // Test1 : First Start
+    TestAosFactory::Start(IMS_SLOT_0);
+    EXPECT_NE(TestAosFactory::GetAosFactory(), nullptr);
+    EXPECT_NE(TestAosFactory::GetLock(), nullptr);
+    EXPECT_NE(TestAosFactory::GetManager(), nullptr);
+    EXPECT_EQ(TestAosFactory::GetManagers().GetSize(), nManagers + 1);
+
+    // Test2 : Duplicate Start
+    TestAosFactory::Start(IMS_SLOT_0);
+    EXPECT_NE(TestAosFactory::GetAosFactory(), nullptr);
+    EXPECT_NE(TestAosFactory::GetLock(), nullptr);
+    EXPECT_NE(TestAosFactory::GetManager(), nullptr);
+    EXPECT_EQ(TestAosFactory::GetManagers().GetSize(), nManagers + 1);
+}
+
+TEST_F(AosFactoryTest, Stop)
+{
+    EXPECT_EQ(TestAosFactory::GetAosFactory(), nullptr);
+    EXPECT_EQ(TestAosFactory::GetLock(), nullptr);
+    EXPECT_EQ(TestAosFactory::GetManager(), nullptr);
+
+    // Test1 : Stop without Start
+    TestAosFactory::Stop(IMS_SLOT_0);
+    EXPECT_NE(TestAosFactory::GetAosFactory(), nullptr);
+    EXPECT_NE(TestAosFactory::GetLock(), nullptr);
+    EXPECT_EQ(TestAosFactory::GetManager(), nullptr);
+
+    // Test2 : Stop after Start
+    TestAosFactory::Start(IMS_SLOT_0);
+    EXPECT_NE(TestAosFactory::GetAosFactory(), nullptr);
+    EXPECT_NE(TestAosFactory::GetLock(), nullptr);
+    EXPECT_NE(TestAosFactory::GetManager(), nullptr);
+    IMS_SINT32 nManagers = TestAosFactory::GetManagers().GetSize();
+
+    TestAosFactory::Stop(IMS_SLOT_0);
+    EXPECT_NE(TestAosFactory::GetAosFactory(), nullptr);
+    EXPECT_NE(TestAosFactory::GetLock(), nullptr);
+    EXPECT_EQ(TestAosFactory::GetManager(), nullptr);
+    EXPECT_EQ(TestAosFactory::GetManagers().GetSize(), nManagers - 1);
+}
