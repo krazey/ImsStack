4fa4d9a4a417d9a3cc30de0161b9d09c97d45d1c
sparvathy <sparvathy@google.com>
2023-02-06 09:54:08 +0000

    (platform) Add unit tests for OsIsim class
    Bug: 267596264
    Test: atest ImsStackNativePlatformTests
    Change-Id: Id48a75128aac44ef1c2bde2c272f76134d2c85c6

---
+++ tests/native/libimsstack/platform/os/android/device/MockIIsimListener.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef MOCK_I_ISIM_LISTENER_H_
+#define MOCK_I_ISIM_LISTENER_H_
+
+#include <gmock/gmock.h>
+
+#include "IIsimListener.h"
+
+class MockIIsimListener : public IIsimListener
+{
+public:
+    inline MockIIsimListener() = default;
+    inline ~MockIIsimListener() = default;
+
+    MOCK_METHOD(void, Isim_OnField, (IN IMS_SINT32 nField, IN const IMSList<ByteArray>& objValues),
+            (override));
+    MOCK_METHOD(void, Isim_OnHomeDomainName, (IN const ByteArray& objDomainName), (override));
+    MOCK_METHOD(void, Isim_OnImpi, (IN const ByteArray& objImpi), (override));
+    MOCK_METHOD(void, Isim_OnImpu, (IN const IMSList<ByteArray>& objImpus), (override));
+    MOCK_METHOD(void, Isim_OnError, (IN IMS_SINT32 nErrorCode), (override));
+    MOCK_METHOD(void, Isim_OnStateChanged, (IN IMS_SINT32 nState), (override));
+};
+
+#endif
