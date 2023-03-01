19f9456dd1183665535f8737c2448522917ffc76
doogler <doogler@google.com>
2023-03-01 14:33:52 +0000

    (Mtc) Update DialogInfo classes
    This commit is to update DialogInfo classes.
    1. Add IDialogInfo interface
    2. Not to have multiple DialogInfo in DialogInfoProxy
    3. Attribute name : display-name to display
    4. +sip.rendering value : false to no
    5. Remove manipulation JniExternalCalls from DialogInfo
    6. Make DialogInfo have getters
    7. Remove dialogs with terminated state
    
    Bug: 224439978
    Test: atest ImsStackNativeTestst
    Change-Id: Ic86015e42417b9cbfea356485e278832ba84e5ff

---
+++ native/libimsstack/enabler/include/mtc/dialogevent/IDialogInfoManager.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef INTERFACE_DIALOG_INFO_MANAGER_H_
+#define INTERFACE_DIALOG_INFO_MANAGER_H_
+
+#include "ImsList.h"
+#include "ImsTypeDef.h"
+
+class AString;
+class Dialog;
+
+class IDialogInfoManager
+{
+public:
+    virtual ~IDialogInfoManager() = default;
+
+    /**
+     * Updates internal dialog-info data by parsing the dialog package..
+     *
+     * @param strEventPackage The dialog package will be parsed.
+     * @return The result of updating.
+     */
+    virtual IMS_RESULT Update(IN const AString& strEventPackage) = 0;
+
+    /**
+     * Gets Dialog list.
+     *
+     * @return The reference of Dialog pointer list.
+     */
+    virtual const ImsList<Dialog*>& GetDialogs() const = 0;
+
+    /**
+     * Gets the state of last created DialogInfo.
+     *
+     * @return The state value of dialog-info element in the last received dialog event package.
+     *         Valid values are
+     *         {@link DialogInfo#STATE_INVALID},
+     *         {@link DialogInfo#STATE_FULL},
+     *         {@link Dialog#State#STATE_PARTIAL}.
+     */
+    virtual IMS_UINT32 GetState() const = 0;
+
+    /**
+     * Gets the version of last created DialogInfo.
+     *
+     * @return The version value of dialog-info element in the last received dialog event package.
+     */
+    virtual IMS_UINT32 GetVersion() const = 0;
+
+    /**
+     * Gets the entity of last created DialogInfo.
+     *
+     * @return The entity value of dialog-info element in the last received dialog event package.
+     */
+    virtual const AString& GetEntity() const = 0;
+};
+
+#endif
