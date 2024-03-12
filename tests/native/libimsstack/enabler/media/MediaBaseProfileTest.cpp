2ba0ce15c6a224854e738331d6ad53c8b5f6ac45
Yongjun Park <yongjunpark@google.com>
2024-03-12 13:39:18 +0000

    (AP_IMS)(Media) atest for MediaBaseProfileTest MediaBaseProfile
    - atest for MediaBaseProfileTest MediaBaseProfile
    
    Bug: 224464618
    Test: atest ImsStackNativeEnablerMediaTests
    Change-Id: Icf87bd66e6e186d39056aa92c0dfc93ec7ad7942

--- tests/native/libimsstack/enabler/media/MediaBaseProfileTest.cpp
+++ tests/native/libimsstack/enabler/media/MediaBaseProfileTest.cpp
@@ -25,6 +25,15 @@ const IMS_UINT32 PAYLOAD_NUM = 100;
 const AString PAYLOAD_TYPE = "EVS";
 const IMS_UINT32 SAMPLEING_RATE = 16000;
 const IMS_SINT32 CHANNEL = 1;
+const IpAddress IP_ADDRESS = IpAddress(AString("255.255.255.255"));
+const IMS_UINT32 DATA_PORT = 1234;
+const IMS_UINT32 CONTROL_PORT = DATA_PORT + 1;
+const AString TRANSPORT_TYPE = "RTP/AVPF";
+const IMS_UINT32 RTCP_INTERVAL = 5000;
+const IMS_SINT32 AS = 2000;
+const IMS_SINT32 RS = 100;
+const IMS_SINT32 RR = 200;
+const MEDIA_DIRECTION DIRECTION = MEDIA_DIRECTION_SEND_RECEIVE;
 
 TEST(MediaBaseProfileTest, TestRtpMapCreation)
 {
@@ -214,4 +223,124 @@ TEST(MediaBaseProfileTest, TestBasePayloadSetRtpMap)
     EXPECT_EQ(basePayload->objRtpMap.nChannel, CHANNEL);
 
     delete basePayload;
+}
+
+TEST(MediaBaseProfileTest, TestMediaBaseProfileCreation)
+{
+    MediaBaseProfile* baseProfile1 = new MediaBaseProfile();
+    EXPECT_EQ(baseProfile1->objIpAddress, IpAddress::IPv6NONE);
+    EXPECT_EQ(baseProfile1->nDataPort, 0);
+    EXPECT_EQ(baseProfile1->nControlPort, 0);
+    EXPECT_EQ(baseProfile1->strTransportType, "RTP/AVP");
+    EXPECT_EQ(baseProfile1->nRtcpInterval, 0);
+    EXPECT_EQ(baseProfile1->nBandwidthAs, 0);
+    EXPECT_EQ(baseProfile1->nBandwidthRs, 0);
+    EXPECT_EQ(baseProfile1->nBandwidthRr, 0);
+    EXPECT_EQ(baseProfile1->eDirection, MEDIA_DIRECTION_INVALID);
+
+    baseProfile1->objIpAddress = IP_ADDRESS;
+    baseProfile1->nDataPort = DATA_PORT;
+    baseProfile1->nControlPort = CONTROL_PORT;
+    baseProfile1->strTransportType = TRANSPORT_TYPE;
+    baseProfile1->nRtcpInterval = RTCP_INTERVAL;
+    baseProfile1->nBandwidthAs = AS;
+    baseProfile1->nBandwidthRs = RS;
+    baseProfile1->nBandwidthRr = RR;
+    baseProfile1->eDirection = DIRECTION;
+
+    MediaBaseProfile* baseProfile2 = new MediaBaseProfile(baseProfile1);
+    EXPECT_EQ(baseProfile2->objIpAddress, IP_ADDRESS);
+    EXPECT_EQ(baseProfile2->nDataPort, DATA_PORT);
+    EXPECT_EQ(baseProfile2->nControlPort, CONTROL_PORT);
+    EXPECT_EQ(baseProfile2->strTransportType, TRANSPORT_TYPE);
+    EXPECT_EQ(baseProfile2->nRtcpInterval, RTCP_INTERVAL);
+    EXPECT_EQ(baseProfile2->nBandwidthAs, AS);
+    EXPECT_EQ(baseProfile2->nBandwidthRs, RS);
+    EXPECT_EQ(baseProfile2->nBandwidthRr, RR);
+    EXPECT_EQ(baseProfile2->eDirection, DIRECTION);
+
+    MediaBaseProfile* baseProfile3 = new MediaBaseProfile(*baseProfile2);
+    EXPECT_EQ(baseProfile3->objIpAddress, IP_ADDRESS);
+    EXPECT_EQ(baseProfile3->nDataPort, DATA_PORT);
+    EXPECT_EQ(baseProfile3->nControlPort, CONTROL_PORT);
+    EXPECT_EQ(baseProfile3->strTransportType, TRANSPORT_TYPE);
+    EXPECT_EQ(baseProfile3->nRtcpInterval, RTCP_INTERVAL);
+    EXPECT_EQ(baseProfile3->nBandwidthAs, AS);
+    EXPECT_EQ(baseProfile3->nBandwidthRs, RS);
+    EXPECT_EQ(baseProfile3->nBandwidthRr, RR);
+    EXPECT_EQ(baseProfile3->eDirection, DIRECTION);
+
+    delete baseProfile1;
+    delete baseProfile2;
+    delete baseProfile3;
+}
+
+TEST(MediaBaseProfileTest, TestMediaBaseProfileAssign)
+{
+    MediaBaseProfile* baseProfile1 = new MediaBaseProfile();
+    MediaBaseProfile* baseProfile2 = new MediaBaseProfile();
+
+    baseProfile1->objIpAddress = IP_ADDRESS;
+    baseProfile1->nDataPort = DATA_PORT;
+    baseProfile1->nControlPort = CONTROL_PORT;
+    baseProfile1->strTransportType = TRANSPORT_TYPE;
+    baseProfile1->nRtcpInterval = RTCP_INTERVAL;
+    baseProfile1->nBandwidthAs = AS;
+    baseProfile1->nBandwidthRs = RS;
+    baseProfile1->nBandwidthRr = RR;
+    baseProfile1->eDirection = DIRECTION;
+
+    *baseProfile2 = *baseProfile1;
+
+    EXPECT_EQ(baseProfile2->objIpAddress, IP_ADDRESS);
+    EXPECT_EQ(baseProfile2->nDataPort, DATA_PORT);
+    EXPECT_EQ(baseProfile2->nControlPort, CONTROL_PORT);
+    EXPECT_EQ(baseProfile2->strTransportType, TRANSPORT_TYPE);
+    EXPECT_EQ(baseProfile2->nRtcpInterval, RTCP_INTERVAL);
+    EXPECT_EQ(baseProfile2->nBandwidthAs, AS);
+    EXPECT_EQ(baseProfile2->nBandwidthRs, RS);
+    EXPECT_EQ(baseProfile2->nBandwidthRr, RR);
+    EXPECT_EQ(baseProfile2->eDirection, DIRECTION);
+
+    delete baseProfile1;
+    delete baseProfile2;
+}
+
+TEST(MediaBaseProfileTest, TestMediaBaseProfileEqualNotEqual)
+{
+    MediaBaseProfile* baseProfile1 = new MediaBaseProfile();
+    MediaBaseProfile* baseProfile2 = new MediaBaseProfile();
+
+    EXPECT_EQ(*baseProfile1, *baseProfile2);
+
+    baseProfile1->objIpAddress = IP_ADDRESS;
+    baseProfile1->nDataPort = DATA_PORT;
+    baseProfile1->nControlPort = CONTROL_PORT;
+    baseProfile1->strTransportType = TRANSPORT_TYPE;
+    baseProfile1->nRtcpInterval = RTCP_INTERVAL;
+    baseProfile1->nBandwidthAs = AS;
+    baseProfile1->nBandwidthRs = RS;
+    baseProfile1->nBandwidthRr = RR;
+    baseProfile1->eDirection = DIRECTION;
+
+    EXPECT_NE(*baseProfile1, *baseProfile2);
+
+    baseProfile2->objIpAddress = IP_ADDRESS;
+    baseProfile2->nDataPort = DATA_PORT;
+    baseProfile2->nControlPort = CONTROL_PORT;
+    baseProfile2->strTransportType = TRANSPORT_TYPE;
+    baseProfile2->nRtcpInterval = RTCP_INTERVAL;
+    baseProfile2->nBandwidthAs = AS;
+    baseProfile2->nBandwidthRs = RS;
+    baseProfile2->nBandwidthRr = RR;
+    baseProfile2->eDirection = DIRECTION;
+
+    EXPECT_EQ(*baseProfile1, *baseProfile2);
+
+    baseProfile2->nControlPort = DATA_PORT;
+
+    EXPECT_NE(*baseProfile1, *baseProfile2);
+
+    delete baseProfile1;
+    delete baseProfile2;
 }
