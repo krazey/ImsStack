8b618224b3cdb43acaa5217aff34177d424f426f
Yongjun Park <yongjunpark@google.com>
2024-06-10 05:39:32 +0000

    (AP_IMS)(Media) create MediaProfileUtil class
    - create MediaProfileUtil class to add the utility method for common media profile.
    
    Bug: 318686886
    Test: MO/MT call
          atest ImsStackNativeEnablerMediaTests
    Change-Id: Ie8d57158201fb8289b8f217561325086c8f9fc9c

---
+++ native/libimsstack/enabler/media/MediaProfileUtil.cpp
@@ -0,0 +1,54 @@
+/**
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "MediaProfileUtil.h"
+
+PUBLIC GLOBAL MEDIA_CONTENT_TYPE MediaProfileUtil::GetMediaType(IN const AString payloadType)
+{
+    if (payloadType.EqualsIgnoreCase("AMR-WB") || payloadType.EqualsIgnoreCase("AMR") ||
+            payloadType.EqualsIgnoreCase("EVS") || payloadType.EqualsIgnoreCase("PCMU") ||
+            payloadType.EqualsIgnoreCase("PCMA") || payloadType.EqualsIgnoreCase("telephone-event"))
+    {
+        return MEDIA_TYPE_AUDIO;
+    }
+    else if (payloadType.EqualsIgnoreCase("T140") || payloadType.EqualsIgnoreCase("RED"))
+    {
+        return MEDIA_TYPE_TEXT;
+    }
+    else if (payloadType.EqualsIgnoreCase("H264") || payloadType.EqualsIgnoreCase("H265"))
+    {
+        return MEDIA_TYPE_VIDEO;
+    }
+    else
+    {
+        return MEDIA_TYPE_INVALID;
+    }
+}
+
+PUBLIC GLOBAL IMS_BOOL MediaProfileUtil::IsAudioType(IN const AString payloadType)
+{
+    return (GetMediaType(payloadType) == MEDIA_TYPE_AUDIO) ? IMS_TRUE : IMS_FALSE;
+}
+
+PUBLIC GLOBAL IMS_BOOL MediaProfileUtil::IsTextType(IN const AString payloadType)
+{
+    return (GetMediaType(payloadType) == MEDIA_TYPE_TEXT) ? IMS_TRUE : IMS_FALSE;
+}
+
+PUBLIC GLOBAL IMS_BOOL MediaProfileUtil::IsVideoType(IN const AString payloadType)
+{
+    return (GetMediaType(payloadType) == MEDIA_TYPE_VIDEO) ? IMS_TRUE : IMS_FALSE;
+}
