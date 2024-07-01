961879942276a55bfcf52bc005fd9a84de3956d8
yongnamcha <yongnamcha@google.com>
2024-07-01 10:28:51 +0000

    (AoS) Refactor IAosDebug to improve code quality and maintainability
    This commit refactors IAosDebug to improve code quality and
    maintainability.
      - Improved readability and type safety by replacing int constants
        with the DebugKey enum.
      - Enhanced search speed by replacing the sUnclearableKeys array
        with a Set collection.
      - Optimized performance for enum type keys by replacing
        LinkedHashMap with EnumMap.
      - Ensured null safety by initializing the mDebugData field at
        creation.
      - Strengthened data encapsulation and prevented external
        modification by adding the getDebugData method to provide an
        immutable view of mDebugData.
      - Improved code stability by adding @NonNull annotations to get,
        put,and putInt methods to prevent null input.
      - Updated javadoc.
    
    Flag: EXEMPT refactor
    Bug: 350385011
    Test: atest
    Change-Id: I681ee3aed350b14e325b98568f8358a9309bd829

--- java/src/com/android/imsstack/enabler/aos/IAosDebug.java
+++ java/src/com/android/imsstack/enabler/aos/IAosDebug.java
@@ -16,64 +16,102 @@
 
 package com.android.imsstack.enabler.aos;
 
-import android.annotation.Nullable;
 import android.app.Activity;
 
-import java.util.LinkedHashMap;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.Collections;
+import java.util.EnumMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 /**
- * This class provides an interface for using AosDebug functions.
+ * Provides an interface for interacting with AoS (Always On Service) debugging features.
+ * This interface allows for initializing and cleaning up debug resources,
+ * managing debug notifications, retrieving debug messages, and handling permission results.
+ * It also defines a {@link DebugData} class for storing and managing debug information.
  */
 public interface IAosDebug {
 
-    int[] sUnClearableKeys = {
-        DebugData.KEY_SUB_ID,
-        DebugData.KEY_REGISTER,
-        DebugData.KEY_REGISTER_TIME,
-        DebugData.KEY_DEREGISTER_TIME,
-        DebugData.KEY_DEREGISTER_REASON,
-        DebugData.KEY_REGISTERED_NETWORK_TYPE,
-        DebugData.KEY_FEATURES,
-        DebugData.KEY_CAPABILITIES
-    };
+    /**
+     * Defines keys used to identify specific debug data items within the {@link DebugData} class.
+     * Each key is associated with a unique integer value for efficient storage and retrieval.
+     */
+    enum DebugKey {
+        SUB_ID(0),
+        REGISTER(1),
+        REGISTER_TIME(2),
+        DEREGISTER_TIME(3),
+        DEREGISTER_REASON(4),
+        REGISTERED_NETWORK_TYPE(5),
+        FEATURES(6),
+        CAPABILITIES(7),
+        DATA_CONNECTION_STATE(8),
+        IP_ADDRESSES(9),
+        INTERFACE_NAME(10),
+        MTU(11),
+        NETWORK_TYPE(12),
+        APN_NAME(13),
+        APN_TYPES(14),
+        APN_ENTRY_NAME(15),
+        PCSCF_ADDRESSES(16),
+        SERVICE_STATE(17),
+        DATA_REG_STATE(18),
+        VOICE_RAT(19),
+        CELLULAR_DATA_RAT(20),
+        LTE_ATTACH_TYPE(21),
+        ROAMING_STATE(22),
+        VOICE_ROAMING_TYPE(23),
+        DATA_ROAMING_TYPE(24),
+        NETWORK_OPERATOR(25),
+        NETWORK_OPERATOR_NUMERIC(26),
+        NETWORK_SUPPORT_VOPS(27),
+        NETWORK_SUPPORT_EMCBS(28),
+        UTRAN_LEVEL(29),
+        UTRAN_DBM(30),
+        EUTRAN_RSRP(31),
+        EUTRAN_RSRQ(32),
+        NGRAN_SSRSRP(33),
+        NGRAN_SSRSRQ(34),
+        WIFI_CONNECTION_STATE(35),
+        WIFI_ADDRESSES(36),
+        WIFI_INTERFACE_NAME(37),
+        WIFI_RSSI(38),
+        WIFI_BSSID(39),
+        WIFI_SSID(40),
+        WIFI_MAC_ADDRESS(41);
 
-    int[] sClearableKeys = {
-        DebugData.KEY_DATA_CONNECTION_STATE,
-        DebugData.KEY_IP_ADDRESSES,
-        DebugData.KEY_INTERFACE_NAME,
-        DebugData.KEY_MTU,
-        DebugData.KEY_NETWORK_TYPE,
-        DebugData.KEY_APN_NAME,
-        DebugData.KEY_APN_TYPES,
-        DebugData.KEY_APN_ENTRY_NAME,
-        DebugData.KEY_PCSCF_ADDRESSES,
-        DebugData.KEY_SERVICE_STATE,
-        DebugData.KEY_DATA_REG_STATE,
-        DebugData.KEY_VOICE_RAT,
-        DebugData.KEY_CELLULAR_DATA_RAT,
-        DebugData.KEY_LTE_ATTACH_TYPE,
-        DebugData.KEY_ROAMING_STATE,
-        DebugData.KEY_VOICE_ROAMING_TYPE,
-        DebugData.KEY_DATA_ROAMING_TYPE,
-        DebugData.KEY_NETWORK_OPERATOR,
-        DebugData.KEY_NETWORK_OPERATOR_NUMERIC,
-        DebugData.KEY_NETWORK_SUPPORT_VOPS,
-        DebugData.KEY_NETWORK_SUPPORT_EMCBS,
-        DebugData.KEY_UTRAN_LEVEL,
-        DebugData.KEY_UTRAN_DBM,
-        DebugData.KEY_EUTRAN_RSRP,
-        DebugData.KEY_EUTRAN_RSRQ,
-        DebugData.KEY_NGRAN_SSRSRP,
-        DebugData.KEY_NGRAN_SSRSRQ,
-        DebugData.KEY_WIFI_CONNECTION_STATE,
-        DebugData.KEY_WIFI_ADDRESSES,
-        DebugData.KEY_WIFI_INTERFACE_NAME,
-        DebugData.KEY_WIFI_RSSI,
-        DebugData.KEY_WIFI_BSSID,
-        DebugData.KEY_WIFI_SSID,
-        DebugData.KEY_WIFI_MAC_ADDRESS
-    };
+        private final int mValue;
+
+        DebugKey(int value) {
+            mValue = value;
+        }
+
+        /**
+         * Returns the integer value associated with this debug key.
+         *
+         * @return The integer value.
+         */
+        public int getValue() {
+            return mValue;
+        }
+    }
+
+     /**
+      * A set of {@link DebugKey}s that should be preserved (not cleared) when the {@code clear()}
+      * method of {@link DebugData} is called.
+      */
+    Set<DebugKey> PRESERVED_KEYS = Set.of(
+            DebugKey.SUB_ID,
+            DebugKey.REGISTER,
+            DebugKey.REGISTER_TIME,
+            DebugKey.DEREGISTER_TIME,
+            DebugKey.DEREGISTER_REASON,
+            DebugKey.REGISTERED_NETWORK_TYPE,
+            DebugKey.FEATURES,
+            DebugKey.CAPABILITIES);
 
     /**
      * Initializes the AosDebug object.
@@ -87,11 +125,16 @@ public interface IAosDebug {
     void cleanup();
 
     /**
-     * This method is used to check whether to display a debug notification.
+     * Shows or dismisses the debug notification based on the following conditions:
+     * - When the debug screen is disabled, the notification is dismissed.
+     * - When the activity is null (invoked internally), or if the permission has been granted,
+     *   the notification channel is created and the notification is sent.
+     * - When the activity is not null and the permission has not been granted,
+     *   the permission is requested.
      *
-     * @param activity Activity requesting permission check.
+     * @param activity The activity requesting permission check, or null if invoked internally.
      */
-    void showOrDismissNotification(Activity activity);
+    void showOrDismissNotification(@Nullable Activity activity);
 
     /**
      * This method is used to inform AosDebug of the information when onRequestPermissionsResult is
@@ -100,8 +143,7 @@ public interface IAosDebug {
      * @param requestCode The request code passed in
      *     {@link Activity#requestPermissions(String[], int)}.
      * @param permissions The requested permissions. Never null.
-     * @param grantResults The grant results for the corresponding permissions
-     *                     which is either
+     * @param grantResults The grant results for the corresponding permissions which is either
      *                     {@link android.content.pm.PackageManager#PERMISSION_GRANTED} or
      *                     {@link android.content.pm.PackageManager#PERMISSION_DENIED}. Never null.
      * @param activity The activity whose visibility requested the permission.
@@ -119,51 +161,9 @@ public interface IAosDebug {
     String getDebugMessage();
 
     /**
-     * This class is a Data class for updating in AosDebug.
+     * A data class for storing and managing debug information in {@link IAosDebug}.
      */
     final class DebugData {
-        public static final int KEY_SUB_ID = 0;
-        public static final int KEY_REGISTER = 1;
-        public static final int KEY_REGISTER_TIME = 2;
-        public static final int KEY_DEREGISTER_TIME = 3;
-        public static final int KEY_DEREGISTER_REASON = 4;
-        public static final int KEY_REGISTERED_NETWORK_TYPE = 5;
-        public static final int KEY_FEATURES = 6;
-        public static final int KEY_CAPABILITIES = 7;
-        public static final int KEY_DATA_CONNECTION_STATE = 8;
-        public static final int KEY_IP_ADDRESSES = 9;
-        public static final int KEY_INTERFACE_NAME = 10;
-        public static final int KEY_MTU = 11;
-        public static final int KEY_NETWORK_TYPE = 12;
-        public static final int KEY_APN_NAME = 13;
-        public static final int KEY_APN_TYPES = 14;
-        public static final int KEY_APN_ENTRY_NAME = 15;
-        public static final int KEY_PCSCF_ADDRESSES = 16;
-        public static final int KEY_SERVICE_STATE = 17;
-        public static final int KEY_DATA_REG_STATE = 18;
-        public static final int KEY_VOICE_RAT = 19;
-        public static final int KEY_CELLULAR_DATA_RAT = 20;
-        public static final int KEY_LTE_ATTACH_TYPE = 21;
-        public static final int KEY_ROAMING_STATE = 22;
-        public static final int KEY_VOICE_ROAMING_TYPE = 23;
-        public static final int KEY_DATA_ROAMING_TYPE = 24;
-        public static final int KEY_NETWORK_OPERATOR = 25;
-        public static final int KEY_NETWORK_OPERATOR_NUMERIC = 26;
-        public static final int KEY_NETWORK_SUPPORT_VOPS = 27;
-        public static final int KEY_NETWORK_SUPPORT_EMCBS = 28;
-        public static final int KEY_UTRAN_LEVEL = 29;
-        public static final int KEY_UTRAN_DBM = 30;
-        public static final int KEY_EUTRAN_RSRP = 31;
-        public static final int KEY_EUTRAN_RSRQ = 32;
-        public static final int KEY_NGRAN_SSRSRP = 33;
-        public static final int KEY_NGRAN_SSRSRQ = 34;
-        public static final int KEY_WIFI_CONNECTION_STATE = 35;
-        public static final int KEY_WIFI_ADDRESSES = 36;
-        public static final int KEY_WIFI_INTERFACE_NAME = 37;
-        public static final int KEY_WIFI_RSSI = 38;
-        public static final int KEY_WIFI_BSSID = 39;
-        public static final int KEY_WIFI_SSID = 40;
-        public static final int KEY_WIFI_MAC_ADDRESS = 41;
 
         public static final String STR_EMPTY = "_";
         public static final String STR_IMS_REGISTERED = "REGISTERED";
@@ -171,36 +171,72 @@ public interface IAosDebug {
         public static final String STR_CONNECTED = "CONNECTED";
         public static final String STR_DISCONNECTED = "DISCONNECTED";
 
-        private final Map<Integer, String> mDebugData;
+        private final Map<DebugKey, String> mDebugData = new EnumMap<>(DebugKey.class);
 
         public DebugData() {
-            mDebugData = new LinkedHashMap<>();
-            for (int i = 0; i < sUnClearableKeys.length; i++) {
-                mDebugData.put(sUnClearableKeys[i], STR_EMPTY);
+            for (DebugKey key : DebugKey.values()) {
+                mDebugData.put(key, STR_EMPTY);
             }
 
             clear();
         }
 
+        /**
+         * Clears the debug data for clearable keys.
+         */
         public void clear() {
-            for (int i = 0; i < sClearableKeys.length; i++) {
-                mDebugData.put(sClearableKeys[i], STR_EMPTY);
+            for (DebugKey key : DebugKey.values()) {
+                if (!PRESERVED_KEYS.contains(key)) {
+                    mDebugData.put(key, STR_EMPTY);
+                }
             }
 
-            mDebugData.put(DebugData.KEY_DATA_CONNECTION_STATE, STR_DISCONNECTED);
-            mDebugData.put(DebugData.KEY_WIFI_CONNECTION_STATE, STR_DISCONNECTED);
+            mDebugData.put(DebugKey.DATA_CONNECTION_STATE, STR_DISCONNECTED);
+            mDebugData.put(DebugKey.WIFI_CONNECTION_STATE, STR_DISCONNECTED);
         }
 
-        public void put(Integer key, String data) {
+        /**
+         * Puts a String value for the specified key in the debug data map.
+         *
+         * @param key  The debug key. Must not be null.
+         * @param data The String value to put.
+         */
+        public void put(@NonNull DebugKey key, String data) {
+            Objects.requireNonNull(key, "key must not be null");
             mDebugData.put(key, data);
         }
 
-        public void putInt(Integer key, int data) {
+        /**
+         * Puts an integer value for the specified key in the debug data map.
+         * The integer value is converted to a String before being stored.
+         *
+         * @param key  The debug key. Must not be null.
+         * @param data The integer value to put.
+         */
+        public void putInt(@NonNull DebugKey key, int data) {
+            Objects.requireNonNull(key, "key must not be null");
             mDebugData.put(key, String.valueOf(data));
         }
 
-        public String get(Integer key) {
+        /**
+         * Gets the String value associated with the specified key from the debug data map.
+         * If the key is not found, the default value {@code STR_EMPTY} ("_") is returned.
+         *
+         * @param key The debug key to retrieve the value for. Must not be null.
+         * @return The String value associated with the key, or {@code STR_EMPTY} if not found.
+         */
+        public String get(@NonNull DebugKey key) {
+            Objects.requireNonNull(key, "key must not be null");
             return mDebugData.getOrDefault(key, STR_EMPTY);
+        }
+
+        /**
+         * Returns an unmodifiable view of the debug data map.
+         *
+         * @return An unmodifiable view of the debug data map.
+         */
+        public Map<DebugKey, String> getDebugData() {
+            return Collections.unmodifiableMap(mDebugData);
         }
     }
 }
