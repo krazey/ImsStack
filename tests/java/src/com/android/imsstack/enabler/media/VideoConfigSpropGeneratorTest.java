788464b599c7d1fed54e3ffc04cb34ba34560764
Junho <jhyoon@google.com>
2024-07-22 00:25:41 +0000

    Fix flakiness in VideoConfigSpropGeneratorTest
    We found testInitAndCleanup in VideoConfigSpropGeneratorTest could fail depending on whether other tests are performed. Modified to update ConfigInterface in SetUp and tearDown to prevent unexpected test failures.
    
    Bug: 353623522
    Flag: EXEMPT TEST_ONLY
    Test: atest VideoConfigSpropGeneratorTest and ImsStackJavaTests
    Change-Id: I9858d06aa248f8c86aa5499670bc20168cb3c78d

--- tests/java/src/com/android/imsstack/enabler/media/VideoConfigSpropGeneratorTest.java
+++ tests/java/src/com/android/imsstack/enabler/media/VideoConfigSpropGeneratorTest.java
@@ -20,19 +20,33 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import com.android.imsstack.base.TestAppContext;
+import com.android.imsstack.core.agents.AgentFactory;
+import com.android.imsstack.core.agents.ConfigInterface;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 @RunWith(JUnit4.class)
 public class VideoConfigSpropGeneratorTest {
 
+    @Mock ConfigInterface mMockConfigInterface;
+
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
+
+        AgentFactory.getInstance().setAgent(
+                ConfigInterface.class, mMockConfigInterface, TestAppContext.SLOT0);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        AgentFactory.getInstance().setAgent(ConfigInterface.class, null, TestAppContext.SLOT0);
     }
 
     @Test
