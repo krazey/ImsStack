8eed0d839dc71042d4cba1a16e4a85a9372af3e0
Yongjun Park <yongjunpark@google.com>
2025-07-01 16:36:50 +0000

    (MTC) cppcheck - literalWithCharPtrCompare
    - cppcheck - literalWithCharPtrCompare
    - to avoid pointer address comparison
    
    Test: atest ImsStackNativeEnablerMtcTests
    Bug: 428944899
    Flag: NONE pre-production
    Change-Id: I7ebff253ff3ce7d5c4909113d5f8a5ed4a726cb5

--- tests/native/libimsstack/enabler/mtc/media/MtcMediaStringUtilsTest.cpp
+++ tests/native/libimsstack/enabler/mtc/media/MtcMediaStringUtilsTest.cpp
@@ -32,247 +32,249 @@ protected:
 TEST_F(MtcMediaStringUtilsTest, ConvertReportType)
 {
     IMS_UINT32 eReportType = REPORT_TYPE::REPORT_SUCCESS;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertReportType(eReportType), "success");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertReportType(eReportType), "success");
 
     eReportType = REPORT_TYPE::REPORT_FAILURE;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertReportType(eReportType), "failure");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertReportType(eReportType), "failure");
 
     eReportType = REPORT_TYPE::REPORT_DATA_RECEIVE_FAILED;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertReportType(eReportType), "data receive failed");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertReportType(eReportType), "data receive failed");
 
     eReportType = REPORT_TYPE::REPORT_DATA_RECEIVE_STARTED;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertReportType(eReportType), "data receive started");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertReportType(eReportType), "data receive started");
 
     eReportType = REPORT_TYPE::REPORT_QOS;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertReportType(eReportType), "qos");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertReportType(eReportType), "qos");
 
     eReportType = REPORT_TYPE::REPORT_CHECK_RADIO_CONNECTION;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertReportType(eReportType), "check radio condition");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertReportType(eReportType), "check radio condition");
 
     eReportType = REPORT_TYPE::REPORT_NW_TONE_RTP_RECEIVE_STARTED;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertReportType(eReportType), "network tone receive started");
+    EXPECT_STREQ(
+            MtcMediaStringUtils::ConvertReportType(eReportType), "network tone receive started");
 
     eReportType = REPORT_TYPE::REPORT_NW_TONE_RTP_RECEIVE_FAILED;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertReportType(eReportType), "network tone receive failed");
+    EXPECT_STREQ(
+            MtcMediaStringUtils::ConvertReportType(eReportType), "network tone receive failed");
 
     eReportType = REPORT_TYPE::REPORT_RECEIVED_DTMF_EVENT;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertReportType(eReportType), "dtmf event received");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertReportType(eReportType), "dtmf event received");
 
     eReportType = REPORT_TYPE::REPORT_MEDIA_DETACH;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertReportType(eReportType), "media detach");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertReportType(eReportType), "media detach");
 
     eReportType = REPORT_TYPE::REPORT_TRIGGER_ANBR_QUERY;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertReportType(eReportType), "trigger anbr query");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertReportType(eReportType), "trigger anbr query");
 
     eReportType = REPORT_TYPE::REPORT_ANBR_NEGOTIATION_RESULT;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertReportType(eReportType), "anbr negotiation result");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertReportType(eReportType), "anbr negotiation result");
 
     eReportType = REPORT_TYPE::REPORT_NOTUSED;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertReportType(eReportType), "not used");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertReportType(eReportType), "not used");
 
     eReportType = REPORT_TYPE::REPORT_NOTUSED + 1;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertReportType(eReportType), "OUT_OF_RANGE");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertReportType(eReportType), "OUT_OF_RANGE");
 
     eReportType = REPORT_TYPE::REPORT_SUCCESS - 1;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertReportType(eReportType), "OUT_OF_RANGE");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertReportType(eReportType), "OUT_OF_RANGE");
 }
 
 TEST_F(MtcMediaStringUtilsTest, ConvertContentType)
 {
     IMS_UINT32 eContentType = MEDIA_TYPE_INVALID;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertContentType(eContentType), "invalid");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertContentType(eContentType), "invalid");
 
     eContentType = MEDIA_TYPE_AUDIO;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertContentType(eContentType), "audio");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertContentType(eContentType), "audio");
 
     eContentType = MEDIA_TYPE_VIDEO;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertContentType(eContentType), "video");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertContentType(eContentType), "video");
 
     eContentType = MEDIA_TYPE_AUDIOVIDEO;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertContentType(eContentType), "audio & video");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertContentType(eContentType), "audio & video");
 
     eContentType = MEDIA_TYPE_TEXT;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertContentType(eContentType), "text");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertContentType(eContentType), "text");
 
     eContentType = MEDIA_TYPE_AUDIOTEXT;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertContentType(eContentType), "audio & text");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertContentType(eContentType), "audio & text");
 
     eContentType = MEDIA_TYPE_VIDEOTEXT;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertContentType(eContentType), "video & text");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertContentType(eContentType), "video & text");
 
     eContentType = MEDIA_TYPE_AUDIOVIDEOTEXT;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertContentType(eContentType), "audio & video & text");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertContentType(eContentType), "audio & video & text");
 
     eContentType = MEDIA_TYPE_NOTUSED;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertContentType(eContentType), "not used");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertContentType(eContentType), "not used");
 
     eContentType = MEDIA_TYPE_INVALID - 1;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertContentType(eContentType), "OUT_OF_RANGE");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertContentType(eContentType), "OUT_OF_RANGE");
 
     eContentType = MEDIA_TYPE_NOTUSED + 1;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertContentType(eContentType), "OUT_OF_RANGE");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertContentType(eContentType), "OUT_OF_RANGE");
 }
 
 TEST_F(MtcMediaStringUtilsTest, ConvertErrorType)
 {
     IMS_SINT32 eErrorType = RtpError::NO_ERROR;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "no error");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "no error");
 
     eErrorType = RtpError::INVALID_PARAM;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "invalid param");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "invalid param");
 
     eErrorType = RtpError::NOT_READY;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "not ready");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "not ready");
 
     eErrorType = RtpError::NO_MEMORY;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "no memory");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "no memory");
 
     eErrorType = RtpError::NO_RESOURCES;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "no resources");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "no resources");
 
     eErrorType = RtpError::PORT_UNAVAILABLE;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "port unavailable");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "port unavailable");
 
     eErrorType = RtpError::REQUEST_NOT_SUPPORTED;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "request not supported");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "request not supported");
 
     eErrorType = RtpError::RESPONSE_WAIT_TIMEOUT;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "response wait timeout");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "response wait timeout");
 
     eErrorType = RtpError::NO_ERROR - 1;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "OUT_OF_RANGE");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "OUT_OF_RANGE");
 
     eErrorType = RtpError::RESPONSE_WAIT_TIMEOUT + 1;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "OUT_OF_RANGE");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertErrorType(eErrorType), "OUT_OF_RANGE");
 }
 
 TEST_F(MtcMediaStringUtilsTest, ConvertNegoType)
 {
     MediaNego::MediaNegoResult eNegoType = MediaNego::MediaNegoResult::NO_ERROR;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertNegoType(eNegoType), "no error");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertNegoType(eNegoType), "no error");
 
     eNegoType = MediaNego::MediaNegoResult::ERROR_INVALID_DESCRIPTOR;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertNegoType(eNegoType), "invalid descriptor");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertNegoType(eNegoType), "invalid descriptor");
 
     eNegoType = MediaNego::MediaNegoResult::ERROR_NO_CODEC_MATCHED;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertNegoType(eNegoType), "no codec matched");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertNegoType(eNegoType), "no codec matched");
 
     eNegoType = MediaNego::MediaNegoResult::ERROR_IP_MISMATCH;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertNegoType(eNegoType), "ip mismatch");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertNegoType(eNegoType), "ip mismatch");
 
     eNegoType = MediaNego::MediaNegoResult::ERROR_NO_AUDIO;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertNegoType(eNegoType), "no audio");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertNegoType(eNegoType), "no audio");
 
     eNegoType = MediaNego::MediaNegoResult::ERROR_NO_VIDEO;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertNegoType(eNegoType), "no video");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertNegoType(eNegoType), "no video");
 
     eNegoType = MediaNego::MediaNegoResult::ERROR_NO_TEXT;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertNegoType(eNegoType), "no text");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertNegoType(eNegoType), "no text");
 }
 
 TEST_F(MtcMediaStringUtilsTest, ConvertPemType)
 {
     PemType ePemType = PemType::NONE;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertPemType(ePemType), "none");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertPemType(ePemType), "none");
 
     ePemType = PemType::SENDRECV;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertPemType(ePemType), "sendrecv");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertPemType(ePemType), "sendrecv");
 
     ePemType = PemType::SENDONLY;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertPemType(ePemType), "sendonly");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertPemType(ePemType), "sendonly");
 
     ePemType = PemType::RECVONLY;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertPemType(ePemType), "recvonly");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertPemType(ePemType), "recvonly");
 
     ePemType = PemType::INACTIVE;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertPemType(ePemType), "inactive");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertPemType(ePemType), "inactive");
 }
 
 TEST_F(MtcMediaStringUtilsTest, ConvertQuality)
 {
     IMS_SINT32 eCodecType = MEDIA_QUALITY_NONE - 1;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertQuality(eCodecType), "OUT_OF_RANGE");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertQuality(eCodecType), "OUT_OF_RANGE");
 
     eCodecType = MEDIA_QUALITY_NONE;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertQuality(eCodecType), "none");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertQuality(eCodecType), "none");
 
     eCodecType = MEDIA_QUALITY_NONE + 1;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertQuality(eCodecType), "valid");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertQuality(eCodecType), "valid");
 
     eCodecType = MEDIA_QUALITY_NOTUSED - 1;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertQuality(eCodecType), "valid");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertQuality(eCodecType), "valid");
 
     eCodecType = MEDIA_QUALITY_NOTUSED;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertQuality(eCodecType), "none");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertQuality(eCodecType), "none");
 
     eCodecType = MEDIA_QUALITY_NOTUSED + 1;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertQuality(eCodecType), "OUT_OF_RANGE");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertQuality(eCodecType), "OUT_OF_RANGE");
 }
 
 TEST_F(MtcMediaStringUtilsTest, ConvertNegoState)
 {
     NEGO_STATE eNegoState = STATE_IDLE;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertNegoState(eNegoState), "idle");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertNegoState(eNegoState), "idle");
 
     eNegoState = STATE_OFFER_RECEIVED;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertNegoState(eNegoState), "offer received");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertNegoState(eNegoState), "offer received");
 
     eNegoState = STATE_OFFER_SENT;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertNegoState(eNegoState), "offer sent");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertNegoState(eNegoState), "offer sent");
 
     eNegoState = STATE_NEGOTIATED;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertNegoState(eNegoState), "negotiated");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertNegoState(eNegoState), "negotiated");
 
     eNegoState = STATE_NOTUSED;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertNegoState(eNegoState), "not used");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertNegoState(eNegoState), "not used");
 }
 
 TEST_F(MtcMediaStringUtilsTest, ConvertDirection)
 {
     IMS_SINT32 eDirection = MEDIA_DIRECTION_INVALID;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertDirection(eDirection), "invalid");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertDirection(eDirection), "invalid");
 
     eDirection = MEDIA_DIRECTION_INACTIVE;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertDirection(eDirection), "inactive");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertDirection(eDirection), "inactive");
 
     eDirection = MEDIA_DIRECTION_RECEIVE;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertDirection(eDirection), "receive");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertDirection(eDirection), "receive");
 
     eDirection = MEDIA_DIRECTION_SEND;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertDirection(eDirection), "send");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertDirection(eDirection), "send");
 
     eDirection = MEDIA_DIRECTION_SEND_RECEIVE;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertDirection(eDirection), "sendrecv");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertDirection(eDirection), "sendrecv");
 
     eDirection = MEDIA_DIRECTION_INVALID - 1;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertDirection(eDirection), "OUT_OF_RANGE");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertDirection(eDirection), "OUT_OF_RANGE");
 
     eDirection = MEDIA_DIRECTION_SEND_RECEIVE + 1;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertDirection(eDirection), "OUT_OF_RANGE");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertDirection(eDirection), "OUT_OF_RANGE");
 }
 
 TEST_F(MtcMediaStringUtilsTest, ConvertProtocolType)
 {
     IMS_SINT32 eProtocolType = MEDIA_PROTOCOL_NONE;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertProtocolType(eProtocolType), "none");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertProtocolType(eProtocolType), "none");
 
     eProtocolType = MEDIA_PROTOCOL_ANY;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertProtocolType(eProtocolType), "any");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertProtocolType(eProtocolType), "any");
 
     eProtocolType = MEDIA_PROTOCOL_RTP;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertProtocolType(eProtocolType), "rtp");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertProtocolType(eProtocolType), "rtp");
 
     eProtocolType = MEDIA_PROTOCOL_RTCP;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertProtocolType(eProtocolType), "rtcp");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertProtocolType(eProtocolType), "rtcp");
 
     eProtocolType = MEDIA_PROTOCOL_NO_CHANGE;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertProtocolType(eProtocolType), "no change");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertProtocolType(eProtocolType), "no change");
 
     eProtocolType = MEDIA_PROTOCOL_NONE - 1;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertProtocolType(eProtocolType), "OUT_OF_RANGE");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertProtocolType(eProtocolType), "OUT_OF_RANGE");
 
     eProtocolType = MEDIA_PROTOCOL_NO_CHANGE + 1;
-    EXPECT_EQ(MtcMediaStringUtils::ConvertProtocolType(eProtocolType), "OUT_OF_RANGE");
+    EXPECT_STREQ(MtcMediaStringUtils::ConvertProtocolType(eProtocolType), "OUT_OF_RANGE");
 }
 
 }  // namespace android
