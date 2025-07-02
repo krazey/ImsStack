b939937246d6e984eece47a339243e2e8f91e69c
Jacob Ryou <jryou@google.com>
2025-07-02 13:25:31 +0900

    (Aos) cppcheck: Removed unused functions in AosFeature.h
    Removed below unused functions:
    AosFeature::InitOriginFeatures()
    AosFeature::GetOriginFeatures()
    
    Removed unused variable - IMS_UINT32 nOriginalFeatures
    
    Bug: 428946057
    Test: gTest
    Flag: EXEMPT refactor
    Change-Id: Ib0eaff43684f2078d7ea022cd243dcecd80fec9b

--- native/libimsstack/enabler/include/aos/provider/AosFeature.h
+++ native/libimsstack/enabler/include/aos/provider/AosFeature.h
@@ -22,8 +22,7 @@ class AosFeature
 {
 public:
     AosFeature() :
-            nFeatures(FEATURE_NONE),
-            nOriginalFeatures(FEATURE_NONE)
+            nFeatures(FEATURE_NONE)
     {
         IMS_TRACE_MEM("AOS_MEM", "AOS_M : AosFeature = %" PFLS_u "/%" PFLS_x, sizeof(AosFeature),
                 this, 0);
@@ -46,9 +45,7 @@ public:
     IMS_BOOL IsFeatureEnabled(IN IMS_UINT32 nFeature) { return (nFeatures & nFeature) == nFeature; }
     IMS_BOOL IsFeatureDisabled(IN IMS_UINT32 nFeature) { return !IsFeatureEnabled(nFeature); }
     void InitFeatures(IN IMS_UINT32 nFeatures) { this->nFeatures = nFeatures; }
-    void InitOriginFeatures(IN IMS_UINT32 nFeatures) { this->nOriginalFeatures = nFeatures; }
     IMS_UINT32 GetFeatures() { return nFeatures; }
-    IMS_UINT32 GetOriginFeatures() { return nOriginalFeatures; }
     virtual void OnFeatureEnabled(IN IMS_UINT32 nFeatures) { (void)nFeatures; }
     virtual void OnFeatureDisabled(IN IMS_UINT32 nFeatures) { (void)nFeatures; }
 
@@ -59,7 +56,6 @@ public:
 
 private:
     IMS_UINT32 nFeatures;
-    IMS_UINT32 nOriginalFeatures;
 };
 
 #endif  // AOS_FEATURE_H_
