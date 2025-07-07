c89d97c926374332cb5038edd0c3dd2f9419f815
Hwangoo Park <hwangoo@google.com>
2025-07-07 05:49:11 +0000

    Save and use a config of last inserted SIM
    This change introduces a new policy for determing which carrier
    configuration to use when booting a device without SIM card.
    A new optional config item is defined, allowing the ImsStack to operate
    based on the configuration of the last inserted SIM. This involves
    saving the most recent configuration and enabling its use upon reboot,
    even if no SIM is present.
    In SIM LOCKED case, the cached config can only be used if the SIM serial
    ID is the same.
    
    Bug: 424310036
    Bug: 430705663
    Flag: EXEMPT bugfix
    Test: atest ImsStackJavaTests
    Test: manual (verified the carrier configuration based on the SIM state
    when the device reboots)
    
    Change-Id: I6a16eeeb273e020db631184deb407df7bdc57820

--- java/src/com/android/imsstack/core/config/CarrierConfig.java
+++ java/src/com/android/imsstack/core/config/CarrierConfig.java
@@ -48,6 +48,10 @@ public class CarrierConfig {
     // The key specifies a list of parent carrier ids from which to inherit configuration values.
     public static final String KEY_IMS_PARENT_CARRIER_IDS_INT_ARRAY =
             "ims.parent_carrier_ids_int_array";
+    // This key is a config that stores the carrier-config of the last inserted SIM and
+    // indicates whether it can be used in the no SIM case.
+    public static final String KEY_IMS_USE_CONFIG_OF_LAST_INSERTED_SIM_WHEN_NO_SIM_BOOL =
+            "ims.use_config_of_last_inserted_sim_when_no_sim_bool";
     // This key is a special carrier config value used to avoid overriding the public carrier
     // config within Assets.
     // The default value is true.
@@ -157,6 +161,7 @@ public class CarrierConfig {
         CarrierConfigManager.Ims.KEY_CAPABILITY_TYPE_PRESENCE_UCE_INT_ARRAY,
         CarrierConfigManager.Ims.KEY_NR_SA_DISABLE_POLICY_INT,
         CarrierConfigManager.Ims.KEY_ALLOW_NON_GLOBAL_PHONE_NUMBER_FORMAT_BOOL,
+        KEY_IMS_USE_CONFIG_OF_LAST_INSERTED_SIM_WHEN_NO_SIM_BOOL,
         KEY_IMS_PARENT_CARRIER_IDS_INT_ARRAY,
         KEY_IMS_OVERRIDE_PUBLIC_CONFIG_BOOL
     };
