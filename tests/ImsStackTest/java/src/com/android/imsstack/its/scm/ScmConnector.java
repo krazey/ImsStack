c15cc1ab027e1dde851d6f7841bd90173c0fac5b
Juho Kim <juhokim@google.com>
2025-11-20 09:09:03 +0000

    (its) Connect to SCM only for presubmit test
    Added "use_scm" instrumentation argument to decide if it connects to the
    SCM or not. It will be used only when running the presubmit test.
    
    Bug: 462273033
    Flag: EXEMPT TEST_ONLY
    Test: atest -c ImsStackTest
    Test: atest -c ImsStackPresubmitTest
    Change-Id: I4c7961f9b2826c8e0eb565ac5a4404fbd2d8430f

---
+++ tests/ImsStackTest/java/src/com/android/imsstack/its/scm/ScmConnector.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.imsstack.its.scm;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import android.os.Bundle;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.android.imsstack.its.core.agents.WifiAgent;
+import com.android.imsstack.util.Log;
+
+import org.junit.rules.ExternalResource;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ScmConnector extends ExternalResource {
+    private static final String KEY_SCM = "use_scm";
+    private static final String VALUE_TRUE = "true";
+
+    private static final int CONTROL_HOST_PORT = 9999;
+    private static final String HOST_IP = "192.168.98.1";
+
+    private final WifiAgent mWifiAgent = WifiAgent.getInstance();
+    private Socket mControlSocket;
+    private DataOutputStream mControlOut;
+    private DataInputStream mControlIn;
+
+    @Override
+    protected void before() throws Throwable {
+        if (!isScmArgumentSet()) {
+            return;
+        }
+
+        openScmConnection();
+    }
+
+    @Override
+    protected void after() {
+        closeScmConnection();
+    }
+
+    private void openScmConnection() {
+        mWifiAgent.waitForWifiConnected();
+        assertTrue("Failed to connect to WiFi", mWifiAgent.isWifiConnected());
+
+        try {
+            Log.i(this, "CONTROL: Attempting to connect to host IP " + HOST_IP
+                    + " on port " + CONTROL_HOST_PORT);
+            mControlSocket = new Socket(HOST_IP, CONTROL_HOST_PORT);
+            mControlOut = new DataOutputStream(mControlSocket.getOutputStream());
+            mControlIn = new DataInputStream(mControlSocket.getInputStream());
+            Log.i(this, "CONTROL: Successfully connected to control server.");
+
+            ScmRequest setupRequest = ScmRequest.newBuilder()
+                    .setAction(Action.START)
+                    .setNumberOfTiss(1)
+                    .build();
+            sendRequest(setupRequest);
+            Log.i(this, "CONTROL: Sent START request");
+
+            ScmResponse setupResponse = receiveResponse();
+            Log.i(this, "CONTROL: Received setup response: '" + setupResponse + "'");
+            assertNotNull("Control server did not respond during setup", setupResponse);
+            assertTrue("Control server returned an error: " + setupResponse.getError(),
+                    setupResponse.getError().isEmpty());
+
+            Log.i(this, "TISS instance started at: " + setupResponse.getTissStatus(0));
+
+        } catch (IOException e) {
+            Log.e(this, "CONTROL: Failed to establish control socket", e);
+            fail("Could not connect to the control server at "
+                    + HOST_IP + ":" + CONTROL_HOST_PORT
+                    + e);
+        }
+    }
+
+    private void closeScmConnection() {
+        if (mControlSocket == null || mControlSocket.isClosed()) {
+            return;
+        }
+
+        try {
+            Log.i(this, "CONTROL: Closing control socket connection");
+
+            ScmRequest stopRequest = ScmRequest.newBuilder()
+                    .setAction(Action.STOP)
+                    .build();
+            sendRequest(stopRequest);
+            Log.i(this, "CONTROL: Sent STOP request");
+
+            ScmResponse teardownResponse = receiveResponse();
+            Log.i(this, "CONTROL: Received teardown response: '" + teardownResponse + "'");
+
+        } catch (IOException e) {
+            Log.e(this, "CONTROL: Error during teardown communication", e);
+
+        } finally {
+            try {
+                if (mControlOut != null) mControlOut.close();
+                if (mControlIn != null) mControlIn.close();
+                mControlSocket.close();
+            } catch (IOException e) {
+                Log.e(this, "CONTROL: Error while closing control socket resources", e);
+            }
+        }
+    }
+
+    private void sendRequest(ScmRequest request) throws IOException {
+        byte[] requestBytes = request.toByteArray();
+        mControlOut.writeInt(requestBytes.length);
+        mControlOut.write(requestBytes);
+        mControlOut.flush();
+    }
+
+    private ScmResponse receiveResponse() throws IOException {
+        int responseLength = mControlIn.readInt();
+        byte[] responseBytes = new byte[responseLength];
+        mControlIn.readFully(responseBytes);
+        return ScmResponse.parseFrom(responseBytes);
+    }
+
+    private boolean isScmArgumentSet() {
+        Bundle args = InstrumentationRegistry.getArguments();
+        if (args == null) {
+            return false;
+        }
+
+        return VALUE_TRUE.equalsIgnoreCase(args.getString(KEY_SCM));
+    }
+}
--- tests/ImsStackTest/java/src/com/android/imsstack/its/tests/ImsTissTest.java
+++ tests/ImsStackTest/java/src/com/android/imsstack/its/tests/ImsTissTest.java
@@ -17,9 +17,7 @@ package com.android.imsstack.its.tests;
 
 import static com.android.imsstack.its.base.TestConstants.SLOT0;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import android.platform.test.annotations.Presubmit;
 import android.testing.AndroidTestingRunner;
@@ -28,9 +26,6 @@ import android.util.Log;
 
 import com.android.imsstack.its.base.TelephonyManagerProxyImpl;
 import com.android.imsstack.its.imsservice.reg.ImsRegistrationWrapper;
-import com.android.imsstack.its.scm.Action;
-import com.android.imsstack.its.scm.ScmRequest;
-import com.android.imsstack.its.scm.ScmResponse;
 import com.android.imsstack.its.servercontrol.ScenarioGeneratorUtils;
 import com.android.imsstack.its.tests.registration.tests.RegistrationTestBase;
 import com.android.imsstack.its.tests.registration.util.TestRegistration;
@@ -41,77 +36,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
-
 @Presubmit
 @RunWith(AndroidTestingRunner.class)
 @TestableLooper.RunWithLooper
 public class ImsTissTest extends RegistrationTestBase {
     private static final String TAG = "ImsTiss";
-    private static final int CONTROL_HOST_PORT = 9999;
-    private static final String HOST_IP = "192.168.98.1";
-    private Socket mControlSocket;
-    private DataOutputStream mControlOut;
-    private DataInputStream mControlIn;
 
     private ImsRegistrationWrapper mImsRegistration;
 
     @Before
     public void setUp() throws Exception {
         Log.i(TAG, "ImsTissTest setup.");
-
-        mWifiAgent.waitForWifiConnected();
-        assertTrue("Failed to connect to WiFi", mWifiAgent.isWifiConnected());
-        Log.i(TAG, "Successfully connected to WiFi.");
-
-        Log.i(TAG, "Setup Connection with tiss_scm and launch TISS.");
-        try {
-            Log.i(
-                    TAG,
-                    "CONTROL: Attempting to connect to Host IP "
-                            + HOST_IP
-                            + " on port "
-                            + CONTROL_HOST_PORT);
-            mControlSocket = new Socket(HOST_IP, CONTROL_HOST_PORT);
-
-            mControlOut = new DataOutputStream(mControlSocket.getOutputStream());
-            mControlIn = new DataInputStream(mControlSocket.getInputStream());
-
-            Log.i(TAG, "CONTROL: Successfully connected to control server.");
-
-            ScmRequest setupRequest =
-                    ScmRequest.newBuilder().setAction(Action.START).setNumberOfTiss(1).build();
-
-            byte[] requestBytes = setupRequest.toByteArray();
-            mControlOut.writeInt(requestBytes.length); // Send 4-byte length prefix
-            mControlOut.write(requestBytes); // Send proto bytes
-            mControlOut.flush();
-            Log.i(TAG, "CONTROL: Sent START request.");
-
-            int responseLength = mControlIn.readInt();
-            byte[] responseBytes = new byte[responseLength];
-            mControlIn.readFully(responseBytes);
-            ScmResponse setupResponse = ScmResponse.parseFrom(responseBytes);
-
-            Log.i(TAG, "CONTROL: Received setup response: '" + setupResponse + "'");
-            assertNotNull("Control server did not respond during setup", setupResponse);
-            assertTrue(
-                    "SCM server returned an error: " + setupResponse.getError(),
-                    setupResponse.getError().isEmpty());
-            Log.i(TAG, "TISS instance started at: " + setupResponse.getTissStatus(0));
-
-        } catch (IOException e) {
-            Log.e(TAG, "CONTROL: Failed to establish control socket in setUp()", e);
-            fail(
-                    "Could not connect to the control server at "
-                            + HOST_IP
-                            + ":"
-                            + CONTROL_HOST_PORT
-                            + e);
-        }
 
         setUpBase(SLOT0);
         mImsRegistration = mImsServiceConnector.getRegistration();
@@ -122,34 +57,8 @@ public class ImsTissTest extends RegistrationTestBase {
     @After
     public void tearDown() throws Exception {
         Log.i(TAG, "ImsTissTest tearDown.");
-        mServerControlConnection.disconnect();
 
         tearDownBase(SLOT0);
-
-        if (mControlSocket != null && !mControlSocket.isClosed()) {
-            try {
-                Log.i(TAG, "CONTROL: Closing control socket connection.");
-
-                ScmRequest stopRequest = ScmRequest.newBuilder().setAction(Action.STOP).build();
-                byte[] requestBytes = stopRequest.toByteArray();
-                mControlOut.writeInt(requestBytes.length);
-                mControlOut.write(requestBytes);
-                mControlOut.flush();
-                Log.i(TAG, "CONTROL: Sent STOP request.");
-
-                int responseLength = mControlIn.readInt();
-                byte[] responseBytes = new byte[responseLength];
-                mControlIn.readFully(responseBytes);
-                ScmResponse teardownResponse = ScmResponse.parseFrom(responseBytes);
-                Log.i(TAG, "CONTROL: Received teardown response: '" + teardownResponse + "'");
-
-                mControlOut.close();
-                mControlIn.close();
-                mControlSocket.close();
-            } catch (IOException e) {
-                Log.e(TAG, "CONTROL: Error while closing control socket", e);
-            }
-        }
     }
 
     @Test
