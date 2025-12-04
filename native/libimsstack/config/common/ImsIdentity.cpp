a06ffdcddff5329ae36c48916b5f64ba8a603c62
Hwangoo Park <hwangoo@google.com>
2025-12-04 05:27:11 +0000

    (config) Fix cppclean - include statement for platform
    This change is to remove cppclean warnings about the include statement.
     - not need to be #included
     - use a forward declaration
    
    Bug: 428836831
    Flag: EXEMPT PURE_REFACTOR
    Test: build, atest ImsStackNativeTests
    Change-Id: I34e1773d96f4b540212f00f3dd6f7721f3336e3e

--- native/libimsstack/config/common/ImsIdentity.cpp
+++ native/libimsstack/config/common/ImsIdentity.cpp
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 #include "AStringBuffer.h"
+#include "IPhoneInfoSubscriber.h"
 #include "ImsAccessNetworkInfoType.h"
 #include "ServiceMemory.h"
 #include "ServicePhoneInfo.h"
